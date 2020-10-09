ee2f23b5937513f542d17412c15e6137047fed4c
Gus Gutoski <gus.gutoski@protonmail.com>
2020-10-08 21:52:40 -0400

    add logging info

--- x/tss/keeper/keeper.go
+++ x/tss/keeper/keeper.go
@@ -2,16 +2,17 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"time"
 
+	"github.com/axelarnetwork/axelar-core/x/tss/types"
 	"github.com/axelarnetwork/tssd/pb"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/tendermint/tendermint/libs/log"
 	"google.golang.org/grpc"
 )
-
-// var (
-// 	_ axTypes.BridgeKeeper = Keeper{}
-// )
 
 type Keeper struct {
 	client       pb.GG18Client // TODO `pb` is not a good package name
@@ -43,7 +44,13 @@ func NewKeeper() (Keeper, error) {
 	}, nil
 }
 
-func (k Keeper) KeygenStart(info *pb.KeygenInfo) error {
+// Logger returns a module-specific logger.
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+func (k Keeper) KeygenStart(ctx sdk.Context, info *pb.KeygenInfo) error {
+	k.Logger(ctx).Debug(fmt.Sprintf("start keygen protocol: %s", info.NewKeyId))
 	_, err := k.client.KeygenInit(k.context, info)
 	if err != nil {
 		return err
@@ -53,23 +60,24 @@ func (k Keeper) KeygenStart(info *pb.KeygenInfo) error {
 		return err
 	}
 
-	// TODO save my info from info.Parties[info.MyPartyIndex] ?
+	// TODO save my info from info.Parties[info.MyPartyIndex]?
 
 	// server handler https://grpc.io/docs/languages/go/basics/#bidirectional-streaming-rpc-1
 	go func() {
 		for {
 			msg, err := k.keygenStream.Recv() // blocking
 			if err == io.EOF {                // output stream closed by server
+				k.Logger(ctx).Debug("stream closed by server")
 				k.keygenStream.CloseSend() // TODO is this the right place to call CloseSend?
 				return
 			}
 			if err != nil {
-				// errCh <- err
-				// t.Errorf("you should never see this: %s", err)
-				// sdkerrors.Wrap(types.ErrConnFailed, fmt.Sprintf("unexpected error when waiting for bitcoin node warmup: %s", err.Error()))
+				newErr := sdkerrors.Wrap(err, "failure to receive streamed message from server")
+				k.Logger(ctx).Error(newErr.Error()) // TODO what to do with this error?
 				return
 			}
 
+			k.Logger(ctx).Debug(fmt.Sprintf("outgoing message:\nbroadcast? %t\nto: %s", msg.IsBroadcast, msg.ToPartyUid))
 			// TODO deliver msg
 			_ = msg
 		}
@@ -78,10 +86,12 @@ func (k Keeper) KeygenStart(info *pb.KeygenInfo) error {
 	return nil
 }
 
-func (k Keeper) KeygenMsg(msg *pb.MessageIn) error {
+func (k Keeper) KeygenMsg(ctx sdk.Context, msg *pb.MessageIn) error {
+	k.Logger(ctx).Debug("incoming message:\nbroadcast? %t\nfrom: %s", msg.IsBroadcast, msg.FromPartyUid)
 	if err := k.keygenStream.Send(msg); err != nil {
-		// log message
-		return err
+		newErr := sdkerrors.Wrap(err, "failure to send streamed message to server")
+		k.Logger(ctx).Error(newErr.Error()) // TODO Logger forces me to throw away error metadata
+		return newErr
 	}
 	return nil
 }
