779f706c8ba5efd40216752d800c3c8801d3bffb
Christian Gorenflo <christian@axelar.network>
2020-10-15 20:20:00 -0400

    added GetProxyCount to exported broadcaster

--- x/broadcast/exported/types.go
+++ x/broadcast/exported/types.go
@@ -6,6 +6,7 @@ type Broadcaster interface {
 	Broadcast(ctx sdk.Context, msgs []ValidatorMsg) error
 	RegisterProxy(ctx sdk.Context, principal sdk.ValAddress, proxy sdk.AccAddress) error
 	GetPrincipal(ctx sdk.Context, proxy sdk.AccAddress) sdk.ValAddress
+	GetProxyCount(ctx sdk.Context) uint32
 
 	// WARNING: Handle with care, this exposes local information that is DIFFERENT for each validator
 	GetLocalPrincipal(ctx sdk.Context) sdk.ValAddress
