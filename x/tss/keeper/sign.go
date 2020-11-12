3b1f49984fd8725a1b1e06a7a37651d7ee8d6f63
Gus Gutoski <ggutoski@users.noreply.github.com>
2020-11-12 14:23:36 -0500

    Debug tss -> dev; fix sign, keygen; improve logging (#77)
    * fix keygen by uncommenting ignore code
    
    * fix sign by uncommenting ignore code, BEWARE tx.go reverted for testing purposes
    
    * remove TODO for GetSign
    
    * restore cli sig marshalling

--- x/tss/keeper/sign.go
+++ x/tss/keeper/sign.go
@@ -18,7 +18,7 @@ import (
 
 // StartSign TODO refactor code copied from StartKeygen
 func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
-	k.Logger(ctx).Info(fmt.Sprintf("initiate StartSign: signature [%s] key [%s] ", info.NewSigID, info.KeyID))
+	k.Logger(ctx).Info(fmt.Sprintf("new Sign: sig_id [%s] key_id [%s] message [%s]", info.NewSigID, info.KeyID, string(info.MsgToSign)))
 
 	// TODO do validity check here, everything else in a separate func win no return value to enforce that we return only nil after the validity check has passed
 	// BEGIN: validity check
@@ -37,7 +37,7 @@ func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
 	// TODO call GetLocalPrincipal only once at launch? need to wait until someone pushes a RegisterProxy message on chain...
 	myAddress := k.broadcaster.GetLocalPrincipal(ctx)
 	if myAddress.Empty() {
-		k.Logger(ctx).Info("my validator address is empty; I must not be a validator; ignore StartSign")
+		k.Logger(ctx).Info("ignore Sign: my validator address is empty so I must not be a validator")
 		return nil
 	}
 
@@ -47,7 +47,7 @@ func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
 		partyUids = append(partyUids, v.OperatorAddress.String())
 	}
 
-	k.Logger(ctx).Debug("initiate tssd gRPC call Sign")
+	// k.Logger(ctx).Debug("initiate tssd gRPC call Sign")
 	var err error
 	k.signStream, err = k.client.Sign(k.context)
 	if err != nil {
@@ -55,7 +55,7 @@ func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
 		k.Logger(ctx).Error(wrapErr.Error())
 		return nil // don't propagate nondeterministic errors
 	}
-	k.Logger(ctx).Debug("successful tssd gRPC call Sign")
+	// k.Logger(ctx).Debug("successful tssd gRPC call Sign")
 
 	// TODO refactor
 	signInfo := &tssd.SignMsgIn{
@@ -69,28 +69,30 @@ func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
 		},
 	}
 
-	k.Logger(ctx).Debug("initiate tssd gRPC sign init goroutine")
+	k.Logger(ctx).Debug(fmt.Sprintf("my uid [%s] of %v", myAddress.String(), partyUids))
+
+	// k.Logger(ctx).Debug("initiate tssd gRPC sign init goroutine")
 	go func(log log.Logger) {
-		log.Debug("sign init goroutine: begin")
-		defer log.Debug("sign init goroutine: end")
+		// log.Debug("sign init goroutine: begin")
+		// defer log.Debug("sign init goroutine: end")
 		if err := k.signStream.Send(signInfo); err != nil {
 			wrapErr := sdkerrors.Wrap(err, "failed tssd gRPC sign send sign init data")
 			log.Error(wrapErr.Error())
 		} else {
-			log.Debug("successful tssd gRPC sign init goroutine")
+			// log.Debug("successful tssd gRPC sign init goroutine")
 		}
 	}(k.Logger(ctx))
 
 	// server handler https://grpc.io/docs/languages/go/basics/#bidirectional-streaming-rpc-1
 	// TODO refactor
-	k.Logger(ctx).Debug("initiate gRPC handler goroutine")
+	// k.Logger(ctx).Debug("initiate gRPC handler goroutine")
 	go func(log log.Logger) {
-		log.Debug("handler goroutine: begin")
+		// log.Debug("handler goroutine: begin")
 		defer func() {
-			log.Debug("handler goroutine: end")
+			// log.Debug("handler goroutine: end")
 		}()
 		for {
-			log.Debug("handler goroutine: blocking call to gRPC stream Recv...")
+			// log.Debug("handler goroutine: blocking call to gRPC stream Recv...")
 			msgOneof, err := k.signStream.Recv() // blocking
 			if err == io.EOF {                   // output stream closed by server
 				log.Debug("handler goroutine: gRPC stream closed by server")
@@ -116,17 +118,16 @@ func (k *Keeper) StartSign(ctx sdk.Context, info types.MsgSignStart) error {
 				log.Error(newErr.Error())
 				return
 			}
-			log.Debug(fmt.Sprintf("handler goroutine: successful sign msg broadcast"))
+			// log.Debug(fmt.Sprintf("handler goroutine: successful sign msg broadcast"))
 		}
 	}(k.Logger(ctx))
 
-	k.Logger(ctx).Debug(fmt.Sprintf("successful StartSign: key [%s] signature [%s]", info.KeyID, info.NewSigID))
+	// k.Logger(ctx).Debug(fmt.Sprintf("successful StartSign: key [%s] signature [%s]", info.KeyID, info.NewSigID))
 	return nil
 }
 
 // SignMsg TODO refactor code copied from keygen
 func (k Keeper) SignMsg(ctx sdk.Context, msg types.MsgSignTraffic) error {
-	k.Logger(ctx).Debug(fmt.Sprintf("initiate SignMsg: sig_id [%s] from broadcaster [%s] broadcast? [%t] to [%s]", msg.SessionID, msg.Sender, msg.Payload.IsBroadcast, msg.Payload.ToPartyUid))
 
 	// TODO many of these checks apply to both keygen and sign; refactor them into a Msg() method
 
@@ -140,12 +141,13 @@ func (k Keeper) SignMsg(ctx sdk.Context, msg types.MsgSignTraffic) error {
 		k.Logger(ctx).Error(err.Error())
 		return err
 	}
+	k.Logger(ctx).Debug(fmt.Sprintf("Sign message: sig [%s] from [%s] to [%s] broadcast? [%t]", msg.SessionID, senderAddress.String(), msg.Payload.ToPartyUid, msg.Payload.IsBroadcast))
 
 	// END: validity check -- always return nil after this line!
 
 	myAddress := k.broadcaster.GetLocalPrincipal(ctx)
 	if myAddress.Empty() {
-		k.Logger(ctx).Info(fmt.Sprintf("ignore message: i'm not a validator; only validators care about messages of type %T", msg))
+		k.Logger(ctx).Info(fmt.Sprintf("ignore Sign message: my validator address is empty so I must not be a validator"))
 		return nil
 	}
 	toAddress, err := sdk.ValAddressFromBech32(msg.Payload.ToPartyUid)
@@ -154,16 +156,19 @@ func (k Keeper) SignMsg(ctx sdk.Context, msg types.MsgSignTraffic) error {
 		k.Logger(ctx).Error(newErr.Error())
 		return nil
 	}
-	k.Logger(ctx).Debug(fmt.Sprintf("myAddress [%s], senderAddress [%s], parsed toAddress [%s]", myAddress, senderAddress, toAddress))
+	if toAddress.String() != msg.Payload.ToPartyUid {
+		k.Logger(ctx).Error("Sign message: address parse discrepancy: given [%s] got [%s]", msg.Payload.ToPartyUid, toAddress.String())
+	}
 	// TODO this ignore code is buggy but I don't know why
-	// if !msg.Payload.IsBroadcast && !myAddress.Equals(toAddress) {
-	// 	k.Logger(ctx).Info(fmt.Sprintf("ignore message: msg to [%s] not directed to me [%s]", toAddress, myAddress))
-	// 	return nil
-	// }
-	// if msg.Payload.IsBroadcast && myAddress.Equals(senderAddress) {
-	// 	k.Logger(ctx).Info(fmt.Sprintf("ignore message: broadcast message from [%s] came from me [%s]", senderAddress, myAddress))
-	// 	return nil
-	// }
+	if !msg.Payload.IsBroadcast && !myAddress.Equals(toAddress) {
+		k.Logger(ctx).Info(fmt.Sprintf("I should ignore: msg to [%s] not directed to me [%s]", toAddress, myAddress))
+		return nil
+	} else if msg.Payload.IsBroadcast && myAddress.Equals(senderAddress) {
+		k.Logger(ctx).Info(fmt.Sprintf("I should ignore: broadcast msg from [%s] came from me [%s]", senderAddress, myAddress))
+		return nil
+	} else {
+		k.Logger(ctx).Info(fmt.Sprintf("I should NOT ignore: msg from [%s] to [%s] broadcast [%t] me [%s]", senderAddress, toAddress, msg.Payload.IsBroadcast, myAddress))
+	}
 
 	// convert the received types.MsgSignTraffic into a tssd.SignMsgIn
 	msgIn := &tssd.SignMsgIn{
@@ -176,27 +181,25 @@ func (k Keeper) SignMsg(ctx sdk.Context, msg types.MsgSignTraffic) error {
 		},
 	}
 
-	k.Logger(ctx).Debug(fmt.Sprintf("initiate forward incoming msg to gRPC server"))
+	// k.Logger(ctx).Debug(fmt.Sprintf("initiate forward incoming msg to gRPC server"))
 	if k.signStream == nil {
 		k.Logger(ctx).Error("nil signStream")
 		return nil // don't propagate nondeterministic errors
 	}
+
+	k.Logger(ctx).Debug(fmt.Sprintf("Sign message: forward incoming to tssd: sig [%s] from [%s] to [%s] broadcast [%t] me [%s]", msg.SessionID, senderAddress.String(), toAddress.String(), msg.Payload.IsBroadcast, myAddress.String()))
+
 	if err := k.signStream.Send(msgIn); err != nil {
 		newErr := sdkerrors.Wrap(err, "failure to send incoming msg to gRPC server")
 		k.Logger(ctx).Error(newErr.Error())
 		return nil // don't propagate nondeterministic errors
 	}
-	k.Logger(ctx).Debug(fmt.Sprintf("successful SignMsg: sig_id [%s] ", msg.SessionID))
+	// k.Logger(ctx).Debug(fmt.Sprintf("successful SignMsg: sig_id [%s] ", msg.SessionID))
 	return nil
 }
 
 // GetSig returns the signature associated with sigID
 // or nil, nil if no such signature exists
-// TODO we need a suiable signature struct
-// Tendermint uses btcd under the hood:
-// https://github.com/tendermint/tendermint/blob/1a8e42d41e9a2a21cb47806a083253ad54c22456/crypto/secp256k1/secp256k1_nocgo.go#L62
-// https://github.com/btcsuite/btcd/blob/535f25593d47297f2c7f27fac7725c3b9b05727d/btcec/signature.go#L25-L29
-// but we don't want to import btcd everywhere
 func (k Keeper) GetSig(ctx sdk.Context, sigUid string) (r *big.Int, s *big.Int, e error) {
 	sigBytes, err := k.client.GetSig(
 		k.context,
