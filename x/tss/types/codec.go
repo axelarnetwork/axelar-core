9d9e90d5a4b1f5c577fa1231318db6eefcc18de6
Christian Gorenflo <christian@axelar.network>
2020-12-15 14:21:12 -0500

    Bugfix unregistered type MsgMasterKeySignStart

--- x/tss/types/codec.go
+++ x/tss/types/codec.go
@@ -14,6 +14,7 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgRotateMasterKey{}, "tss/MsgRotateMasterKey", nil)
 	cdc.RegisterConcrete(&MsgVoteSig{}, "tss/MsgVoteSig", nil)
 	cdc.RegisterConcrete(&MsgVotePubKey{}, "tss/MsgVotePubKey", nil)
+	cdc.RegisterConcrete(MsgMasterKeySignStart{}, "tss/MsgMasterKeySignStart", nil)
 
 	// this module's votes contain byte slices and for the VotingData interface
 	cdc.RegisterConcrete([]byte{}, "tss/bytes", nil)
