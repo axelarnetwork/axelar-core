8518e90248fcd9604c5389cc91f50e9bf46603d8
João Sousa <joaosousa@MBP-de-Joao.home>
2021-02-09 22:00:02 +0000

    Tweeks to the balance keeper

--- x/balance/keeper/keeper.go
+++ x/balance/keeper/keeper.go
@@ -187,11 +187,11 @@ func (k Keeper) subChainTotal(ctx sdk.Context, chain exported.Chain, amount sdk.
 	ctx.KVStore(k.storeKey).Set([]byte(totalPrefix+chain.String()+"_"+amount.Denom), k.cdc.MustMarshalBinaryLengthPrefixed(total))
 }
 
-func (k Keeper) subChainTotal(ctx sdk.Context, chain exported.Chain, amount sdk.Coin) {
-	total := k.getChainTotal(ctx, chain, amount.Denom)
-	total = sdk.NewCoin(amount.Denom, total.Amount.Sub(amount.Amount))
+func (k Keeper) subChainTotal(ctx sdk.Context, chain exported.Chain, withdrawl sdk.Coin) {
+	total := k.getChainTotal(ctx, chain, withdrawl.Denom)
+	total = total.Sub(withdrawl)
 
-	ctx.KVStore(k.storeKey).Set([]byte(totalPrefix+chain.String()+"_"+amount.Denom), k.cdc.MustMarshalBinaryLengthPrefixed(total))
+	ctx.KVStore(k.storeKey).Set([]byte(totalPrefix+chain.String()+"_"+withdrawl.Denom), k.cdc.MustMarshalBinaryLengthPrefixed(total))
 }
 
 func (k Keeper) setPendingTransfer(ctx sdk.Context, recipient exported.CrossChainAddress, amount sdk.Coin) {
