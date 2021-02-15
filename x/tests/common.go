f33d130eff8b13bfa10bbfcdc7d4cc14e035c620
Stelios Daveas <sdaveas@gmail.com>
2021-02-15 20:41:55 +0200

    test: put newNode to common

--- x/tests/common.go
+++ x/tests/common.go
@@ -7,16 +7,40 @@ import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	sdkExported "github.com/cosmos/cosmos-sdk/x/staking/exported"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
 	"google.golang.org/grpc"
 
+	balanceKeeper "github.com/axelarnetwork/axelar-core/x/balance/keeper"
+	balanceTypes "github.com/axelarnetwork/axelar-core/x/balance/types"
+	"github.com/axelarnetwork/axelar-core/x/ethereum"
+
+	"github.com/axelarnetwork/axelar-core/store"
 	"github.com/axelarnetwork/axelar-core/testutils"
+	"github.com/axelarnetwork/axelar-core/testutils/fake"
+	"github.com/axelarnetwork/axelar-core/x/bitcoin"
+	btcKeeper "github.com/axelarnetwork/axelar-core/x/bitcoin/keeper"
 	btcTypes "github.com/axelarnetwork/axelar-core/x/bitcoin/types"
 	btcMock "github.com/axelarnetwork/axelar-core/x/bitcoin/types/mock"
+	"github.com/axelarnetwork/axelar-core/x/broadcast"
+	broadcastTypes "github.com/axelarnetwork/axelar-core/x/broadcast/types"
+	ethKeeper "github.com/axelarnetwork/axelar-core/x/ethereum/keeper"
+	ethTypes "github.com/axelarnetwork/axelar-core/x/ethereum/types"
 	ethMock "github.com/axelarnetwork/axelar-core/x/ethereum/types/mock"
+	"github.com/axelarnetwork/axelar-core/x/snapshot"
+	snapshotKeeper "github.com/axelarnetwork/axelar-core/x/snapshot/keeper"
+	snapTypes "github.com/axelarnetwork/axelar-core/x/snapshot/types"
 	snapMock "github.com/axelarnetwork/axelar-core/x/snapshot/types/mock"
+	"github.com/axelarnetwork/axelar-core/x/tss"
+	tssKeeper "github.com/axelarnetwork/axelar-core/x/tss/keeper"
+	tssTypes "github.com/axelarnetwork/axelar-core/x/tss/types"
 	tssdMock "github.com/axelarnetwork/axelar-core/x/tss/types/mock"
+	"github.com/axelarnetwork/axelar-core/x/vote"
+	voteKeeper "github.com/axelarnetwork/axelar-core/x/vote/keeper"
+	voteTypes "github.com/axelarnetwork/axelar-core/x/vote/types"
 )
 
 func randomSender2(validators2 []staking.Validator, validatorsCount int64) sdk.AccAddress {
@@ -30,6 +54,68 @@ type testMocks2 struct {
 	Sign   *tssdMock.TSSDSignClientMock
 	Staker *snapMock.StakingKeeperMock
 	TSSD   *tssdMock.TSSDClientMock
+}
+
+func newNode2(moniker string, validator sdk.ValAddress, mocks testMocks2, chain *fake.BlockChain) fake.Node {
+	ctx := sdk.NewContext(fake.NewMultiStore(), abci.Header{}, false, log.TestingLogger())
+
+	broadcaster := fake.NewBroadcaster(testutils.Codec(), validator, chain.Submit)
+
+	snapKeeper := snapshotKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(snapTypes.StoreKey), mocks.Staker)
+	voter := voteKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(voteTypes.StoreKey), store.NewSubjectiveStore(), snapKeeper, broadcaster)
+
+	btcSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "btc")
+	bitcoinKeeper := btcKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(btcTypes.StoreKey), btcSubspace)
+	btcParams := btcTypes.DefaultParams()
+	btcParams.Network = mocks.BTC.Network()
+	bitcoinKeeper.SetParams(ctx, btcParams)
+
+	ethSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "eth")
+	ethereumKeeper := ethKeeper.NewEthKeeper(testutils.Codec(), sdk.NewKVStoreKey(ethTypes.StoreKey), ethSubspace)
+	ethParams := ethTypes.DefaultParams()
+	// ethParams.Network = mocks.ETH.Network()
+	ethereumKeeper.SetParams(ctx, ethParams)
+
+	signer := tssKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(tssTypes.StoreKey), mocks.TSSD,
+		params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("storeKey"), sdk.NewKVStoreKey("tstorekey"), tssTypes.DefaultParamspace),
+		voter, broadcaster,
+	)
+	signer.SetParams(ctx, tssTypes.DefaultParams())
+
+	balanceSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("balanceKey"), sdk.NewKVStoreKey("tbalanceKey"), "balance")
+	balancer := balanceKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(balanceTypes.StoreKey), balanceSubspace)
+	balancer.SetParams(ctx, balanceTypes.DefaultParams())
+
+	voter.SetVotingInterval(ctx, voteTypes.DefaultGenesisState().VotingInterval)
+	voter.SetVotingThreshold(ctx, voteTypes.DefaultGenesisState().VotingThreshold)
+
+	router := fake.NewRouter()
+
+	broadcastHandler := broadcast.NewHandler(broadcaster)
+	btcHandler := bitcoin.NewHandler(bitcoinKeeper, voter, mocks.BTC, signer, snapKeeper, balancer)
+	ethHandler := ethereum.NewHandler(ethereumKeeper, mocks.ETH, voter, signer, snapKeeper, balancer)
+	snapHandler := snapshot.NewHandler(snapKeeper)
+	tssHandler := tss.NewHandler(signer, snapKeeper, voter)
+	voteHandler := vote.NewHandler()
+
+	router = router.
+		AddRoute(broadcastTypes.RouterKey, broadcastHandler).
+		AddRoute(btcTypes.RouterKey, btcHandler).
+		AddRoute(ethTypes.RouterKey, ethHandler).
+		AddRoute(snapTypes.RouterKey, snapHandler).
+		AddRoute(voteTypes.RouterKey, voteHandler).
+		AddRoute(tssTypes.RouterKey, tssHandler)
+
+	queriers := map[string]sdk.Querier{
+		btcTypes.QuerierRoute: btcKeeper.NewQuerier(bitcoinKeeper, signer, balancer, mocks.BTC),
+		ethTypes.QuerierRoute: ethKeeper.NewQuerier(mocks.ETH, ethereumKeeper, signer),
+	}
+
+	node := fake.NewNode(moniker, ctx, router, queriers).
+		WithEndBlockers(func(ctx sdk.Context, req abci.RequestEndBlock) []abci.ValidatorUpdate {
+			return vote.EndBlocker(ctx, req, voter)
+		})
+	return node
 }
 
 func createMocks2(validators2 *[]staking.Validator) testMocks2 {
--- x/tests/wBTCMint_test.go
+++ x/tests/wBTCMint_test.go
@@ -11,33 +11,16 @@ import (
 
 	goEth "github.com/ethereum/go-ethereum"
 
-	"github.com/axelarnetwork/axelar-core/store"
 	"github.com/axelarnetwork/axelar-core/testutils"
 	"github.com/axelarnetwork/axelar-core/testutils/fake"
 	balance "github.com/axelarnetwork/axelar-core/x/balance/exported"
-	balanceKeeper "github.com/axelarnetwork/axelar-core/x/balance/keeper"
-	balanceTypes "github.com/axelarnetwork/axelar-core/x/balance/types"
-	"github.com/axelarnetwork/axelar-core/x/bitcoin"
 	btcKeeper "github.com/axelarnetwork/axelar-core/x/bitcoin/keeper"
 	btcTypes "github.com/axelarnetwork/axelar-core/x/bitcoin/types"
-	btcMock "github.com/axelarnetwork/axelar-core/x/bitcoin/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/broadcast"
 	broadcastTypes "github.com/axelarnetwork/axelar-core/x/broadcast/types"
-	"github.com/axelarnetwork/axelar-core/x/ethereum"
 	ethKeeper "github.com/axelarnetwork/axelar-core/x/ethereum/keeper"
 	ethTypes "github.com/axelarnetwork/axelar-core/x/ethereum/types"
-	ethMock "github.com/axelarnetwork/axelar-core/x/ethereum/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/snapshot"
-	snapshotKeeper "github.com/axelarnetwork/axelar-core/x/snapshot/keeper"
 	snapTypes "github.com/axelarnetwork/axelar-core/x/snapshot/types"
-	snapMock "github.com/axelarnetwork/axelar-core/x/snapshot/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/tss"
-	tssKeeper "github.com/axelarnetwork/axelar-core/x/tss/keeper"
 	tssTypes "github.com/axelarnetwork/axelar-core/x/tss/types"
-	tssdMock "github.com/axelarnetwork/axelar-core/x/tss/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/vote"
-	voteKeeper "github.com/axelarnetwork/axelar-core/x/vote/keeper"
-	voteTypes "github.com/axelarnetwork/axelar-core/x/vote/types"
 	"github.com/axelarnetwork/tssd/convert"
 	tssd "github.com/axelarnetwork/tssd/pb"
 	"github.com/btcsuite/btcd/btcec"
@@ -45,14 +28,10 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/cosmos/cosmos-sdk/x/staking"
-	sdkExported "github.com/cosmos/cosmos-sdk/x/staking/exported"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
 	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/log"
-	"google.golang.org/grpc"
 )
 
 // 0. Create and start a chain
@@ -338,68 +317,4 @@ func Test_wBTC_mint(t *testing.T) {
 				})},
 	)
 	assert.NoError(t, err)
-	println("7: ok")
-}
-
-
-func newNode2(moniker string, validator sdk.ValAddress, mocks testMocks2, chain *fake.BlockChain) fake.Node {
-	ctx := sdk.NewContext(fake.NewMultiStore(), abci.Header{}, false, log.TestingLogger())
-
-	broadcaster := fake.NewBroadcaster(testutils.Codec(), validator, chain.Submit)
-
-	snapKeeper := snapshotKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(snapTypes.StoreKey), mocks.Staker)
-	voter := voteKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(voteTypes.StoreKey), store.NewSubjectiveStore(), snapKeeper, broadcaster)
-
-	btcSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "btc")
-	bitcoinKeeper := btcKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(btcTypes.StoreKey), btcSubspace)
-	btcParams := btcTypes.DefaultParams()
-	btcParams.Network = mocks.BTC.Network()
-	bitcoinKeeper.SetParams(ctx, btcParams)
-
-	ethSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "eth")
-	ethereumKeeper := ethKeeper.NewEthKeeper(testutils.Codec(), sdk.NewKVStoreKey(ethTypes.StoreKey), ethSubspace)
-	ethParams := ethTypes.DefaultParams()
-	// ethParams.Network = mocks.ETH.Network()
-	ethereumKeeper.SetParams(ctx, ethParams)
-
-	signer := tssKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(tssTypes.StoreKey), mocks.TSSD,
-		params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("storeKey"), sdk.NewKVStoreKey("tstorekey"), tssTypes.DefaultParamspace),
-		voter, broadcaster,
-	)
-	signer.SetParams(ctx, tssTypes.DefaultParams())
-
-	balanceSubspace := params.NewSubspace(testutils.Codec(), sdk.NewKVStoreKey("balanceKey"), sdk.NewKVStoreKey("tbalanceKey"), "balance")
-	balancer := balanceKeeper.NewKeeper(testutils.Codec(), sdk.NewKVStoreKey(balanceTypes.StoreKey), balanceSubspace)
-	balancer.SetParams(ctx, balanceTypes.DefaultParams())
-
-	voter.SetVotingInterval(ctx, voteTypes.DefaultGenesisState().VotingInterval)
-	voter.SetVotingThreshold(ctx, voteTypes.DefaultGenesisState().VotingThreshold)
-
-	router := fake.NewRouter()
-
-	broadcastHandler := broadcast.NewHandler(broadcaster)
-	btcHandler := bitcoin.NewHandler(bitcoinKeeper, voter, mocks.BTC, signer, snapKeeper, balancer)
-	ethHandler := ethereum.NewHandler(ethereumKeeper, mocks.ETH, voter, signer, snapKeeper, balancer)
-	snapHandler := snapshot.NewHandler(snapKeeper)
-	tssHandler := tss.NewHandler(signer, snapKeeper, voter)
-	voteHandler := vote.NewHandler()
-
-	router = router.
-		AddRoute(broadcastTypes.RouterKey, broadcastHandler).
-		AddRoute(btcTypes.RouterKey, btcHandler).
-		AddRoute(ethTypes.RouterKey, ethHandler).
-		AddRoute(snapTypes.RouterKey, snapHandler).
-		AddRoute(voteTypes.RouterKey, voteHandler).
-		AddRoute(tssTypes.RouterKey, tssHandler)
-
-	queriers := map[string]sdk.Querier{
-		btcTypes.QuerierRoute: btcKeeper.NewQuerier(bitcoinKeeper, signer, balancer, mocks.BTC),
-		ethTypes.QuerierRoute: ethKeeper.NewQuerier(mocks.ETH, ethereumKeeper, signer),
-	}
-
-	node := fake.NewNode(moniker, ctx, router, queriers).
-		WithEndBlockers(func(ctx sdk.Context, req abci.RequestEndBlock) []abci.ValidatorUpdate {
-			return vote.EndBlocker(ctx, req, voter)
-		})
-	return node
 }
