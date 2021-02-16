7a5103672e6d62a3ce7f37fef3278b351f0c772d
Stelios Daveas <sdaveas@gmail.com>
2021-02-16 15:26:49 +0200

    test: wrap some more steps in function
    setTssdMock
    createMasterKeyID
    assignMasterKey and
    rotateMasterKey

--- x/tests/commonFunctions.go
+++ x/tests/commonFunctions.go
@@ -1,14 +1,22 @@
 package tests
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/rand"
 	"strconv"
 	"testing"
 	"time"
 
 	"github.com/axelarnetwork/axelar-core/testutils"
 	"github.com/axelarnetwork/axelar-core/testutils/fake"
+	balance "github.com/axelarnetwork/axelar-core/x/balance/exported"
 	broadcastTypes "github.com/axelarnetwork/axelar-core/x/broadcast/types"
 	snapTypes "github.com/axelarnetwork/axelar-core/x/snapshot/types"
+	tssTypes "github.com/axelarnetwork/axelar-core/x/tss/types"
+	"github.com/axelarnetwork/tssd/convert"
+	tssd "github.com/axelarnetwork/tssd/pb"
+	"github.com/btcsuite/btcd/btcec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/stretchr/testify/assert"
@@ -61,5 +69,104 @@ func registerProxies(chain *fake.BlockChain,
 // takeSnapshot takes a snapshot of the current validators
 func takeSnapshot(chain *fake.BlockChain, validators []staking.Validator, nodeCount int64, t *testing.T) {
 	res := <-chain.Submit(snapTypes.MsgSnapshot{Sender: randomSender(validators, nodeCount)})
+	assert.NoError(t, res.Error)
+}
+
+// setTssdMock sets up tssd mock for btc keygen
+func setTssdMock() *ecdsa.PrivateKey {
+	// set up tssd mock for btc keygen
+	btcMasterKey, err := ecdsa.GenerateKey(btcec.S256(), rand.Reader)
+	if err != nil {
+		panic(err)
+	}
+	return btcMasterKey
+}
+
+// createMasterKeyID creates a master key ID and guarantees that .Send and
+// .Close are called by all nodes
+func createMasterKeyID(
+	chain *fake.BlockChain,
+	validators []staking.Validator,
+	nodeCount int,
+	stringGen *testutils.RandDistinctStringGen,
+	mocks testMocks,
+	t *testing.T) (string, *ecdsa.PrivateKey) {
+
+	masterKey := setTssdMock()
+
+	mocks.Keygen.RecvFunc = func() (*tssd.MessageOut, error) {
+		pk, _ := convert.PubkeyToBytes(masterKey.PublicKey)
+		return &tssd.MessageOut{
+			Data: &tssd.MessageOut_KeygenResult{KeygenResult: pk}}, nil
+	}
+
+	oldSendCount := len(mocks.Keygen.SendCalls())
+	oldCloseCount := len(mocks.Keygen.CloseSendCalls())
+
+	// ensure all nodes call .Send()
+	sendTimeout, sendCancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	mocks.Keygen.SendFunc = func(_ *tssd.MessageIn) error {
+		if len(mocks.Keygen.SendCalls()) == nodeCount {
+			sendCancel()
+		}
+		return nil
+	}
+
+	// ensure all nodes call .Close()
+	closeTimeout, closeCancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	mocks.Keygen.CloseSendFunc = func() error {
+		if len(mocks.Keygen.CloseSendCalls()) == nodeCount {
+			closeCancel()
+		}
+		return nil
+	}
+
+	// create btc key
+	masterKeyID := stringGen.Next()
+	res := <-chain.Submit(tssTypes.MsgKeygenStart{
+		Sender:    randomSender(validators, int64(nodeCount)),
+		NewKeyID:  masterKeyID,
+		Threshold: int(testutils.RandIntBetween(1, int64(len(validators)))),
+	})
+	assert.NoError(t, res.Error)
+
+	// assert tssd was properly called
+	<-sendTimeout.Done()
+	<-closeTimeout.Done()
+	assert.Equal(t, nodeCount, len(mocks.Keygen.SendCalls())-oldSendCount)
+	assert.Equal(t, nodeCount, len(mocks.Keygen.CloseSendCalls())-oldCloseCount)
+
+	return masterKeyID, masterKey
+}
+
+// assignMasterKey assigns a master key of a chain
+func assignMasterKey(
+	chain *fake.BlockChain,
+	validators []staking.Validator,
+	nodeCount int64,
+	masterKeyID string,
+	balanceChain balance.Chain,
+	t *testing.T) {
+	// assign bitcoin master key
+	res := <-chain.Submit(tssTypes.MsgAssignNextMasterKey{
+		Sender: randomSender(validators, nodeCount),
+		Chain:  balanceChain,
+		KeyID:  masterKeyID,
+	})
+	assert.NoError(t, res.Error)
+}
+
+// rotateMasterKey rotates a master key of a chain
+func rotateMasterKey(
+	chain *fake.BlockChain,
+	validators []staking.Validator,
+	nodeCount int64,
+	balanceChain balance.Chain,
+	t *testing.T) {
+
+	res := <-chain.Submit(tssTypes.MsgRotateMasterKey{
+		Sender: randomSender(validators, nodeCount),
+		Chain:  balanceChain,
+	})
 	assert.NoError(t, res.Error)
 }
