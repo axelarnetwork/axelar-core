2e831a3eb9450303692fc60e73eb4e49523cd8dd
Sammy Liu <sammy.liu@axelar.network>
2021-02-25 06:20:10 +0800

    fix: start ethereum confirmation height from 1 (#292)

--- x/ethereum/types/params.go
+++ x/ethereum/types/params.go
@@ -100,7 +100,7 @@ func validateConfirmationHeight(height interface{}) error {
 	if !ok {
 		return fmt.Errorf("invalid parameter type for confirmation height: %T", height)
 	}
-	if h < 0 {
+	if h < 1 {
 		return sdkerrors.Wrap(types.ErrInvalidGenesis, "transaction confirmation height must be greater than 0")
 	}
 	return nil
