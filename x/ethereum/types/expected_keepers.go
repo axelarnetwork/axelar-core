ba7fce73792050ddee36de70e3b0a2a055326a8d
Sergey G <sgorbunov100@gmail.com>
2021-02-23 19:36:09 -0500

    fix(ethereum): removing comments code

--- x/ethereum/types/expected_keepers.go
+++ x/ethereum/types/expected_keepers.go
@@ -40,12 +40,3 @@ type Signer interface {
 	GetKeyForSigID(ctx sdk.Context, sigID string) (ecdsa.PublicKey, bool)
 	GetSnapshotCounterForKeyID(ctx sdk.Context, keyID string) (int64, bool)
 }
-
-// Snapshotter provides snapshot functionality
-/*type Snapshotter interface {
-	GetValidator(ctx sdk.Context, address sdk.ValAddress) (snapshot.Validator, bool)
-	GetLatestSnapshot(ctx sdk.Context) (snapshot.Snapshot, bool)
-	GetLatestCounter(ctx sdk.Context) int64
-	GetSnapshot(ctx sdk.Context, counter int64) (snapshot.Snapshot, bool)
-	TakeSnapshot(ctx sdk.Context) error
-}*/
