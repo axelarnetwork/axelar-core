049c624aecd2f4a8ce71cd5a0ec07a11980473e5
jack <jackson.virgo1@gmail.com>
2021-04-28 01:13:52 +0930

    refactor(events): add Consume function

---
+++ cmd/axelard/cmd/vald/events/events.go
@@ -0,0 +1,35 @@
+package events
+
+import (
+	"fmt"
+	"github.com/axelarnetwork/axelar-core/cmd/axelard/cmd/vald/jobs"
+	"github.com/axelarnetwork/tm-events/pkg/tendermint/events"
+	"github.com/cosmos/cosmos-sdk/types"
+)
+
+// Consume processes all events from the given subscriber with the given function.
+// Do not consume the same subscriber multiple times.
+func Consume(subscriber events.FilteredSubscriber, process func(attributes []types.Attribute) error) jobs.Job {
+	return func(errChan chan<- error) {
+	loop:
+		for {
+			select {
+			case e := <-subscriber.Events():
+				go func() {
+					defer recovery(errChan)
+					if err := process(e.Attributes); err != nil {
+						errChan <- err
+					}
+				}()
+			case <-subscriber.Done():
+				break loop
+			}
+		}
+	}
+}
+
+func recovery(errChan chan<- error) {
+	if r := recover(); r != nil {
+		errChan <- fmt.Errorf("job panicked:%s", r)
+	}
+}
