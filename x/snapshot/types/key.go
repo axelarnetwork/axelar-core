b551b82c9df18267a455464163974b99bea4f701
Christian Gorenflo <christian@axelar.network>
2021-05-14 19:34:35 -0400

    refactor(snapshot): remove handler (#479)

--- x/snapshot/types/key.go
+++ x/snapshot/types/key.go
@@ -7,9 +7,4 @@ const (
 
 	// StoreKey to be used when creating the KVStore
 	StoreKey = ModuleName
-
-	// RouterKey to be used for routing msgs
-	RouterKey = ModuleName
-
-	QuerierRoute = ModuleName
 )
