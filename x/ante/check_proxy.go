162b503b8993e5c1ece2273c1fbdb042408bc8d6
jcs47 <joao@axelar.network>
2021-11-15 20:02:03 +0000

    fix(ante): proxy error msg (#958)

--- x/ante/check_proxy.go
+++ x/ante/check_proxy.go
@@ -7,20 +7,20 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-// CheckProxyReadiness checks if the proxy already sent its readiness message
-type CheckProxyReadiness struct {
+// CheckProxy checks if the proxy already sent its readiness message
+type CheckProxy struct {
 	snapshotter types.Snapshotter
 }
 
-// NewCheckProxyReadiness constructor for CheckProxyReadiness
-func NewCheckProxyReadiness(snapshotter types.Snapshotter) CheckProxyReadiness {
-	return CheckProxyReadiness{
+// NewCheckProxy constructor for CheckProxyReadiness
+func NewCheckProxy(snapshotter types.Snapshotter) CheckProxy {
+	return CheckProxy{
 		snapshotter,
 	}
 }
 
 // AnteHandle fails the transaction if it finds any validator holding tss share of active keys is trying to unbond
-func (d CheckProxyReadiness) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+func (d CheckProxy) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	// exempt genesis validator(s) from this check
 	if ctx.BlockHeight() == 0 {
 		return next(ctx, tx, simulate)
@@ -34,8 +34,8 @@ func (d CheckProxyReadiness) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate boo
 			if err != nil {
 				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, err.Error())
 			}
-			if !d.snapshotter.IsProxyReady(ctx, valAddress) {
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "no readiness message found for operator %s", valAddress.String())
+			if proxy, active := d.snapshotter.GetProxy(ctx, valAddress); proxy.Empty() || !active {
+				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "no proxy found for operator %s", valAddress.String())
 			}
 		default:
 			continue
