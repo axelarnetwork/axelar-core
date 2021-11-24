399d2fa043cb9799d183eebcaf6a550420582e57
Gus Gutoski <ggutoski@users.noreply.github.com>
2021-11-24 16:43:57 -0500

    feat: enable ledger support in axelard (#987) (#1007)
    * feat: enable ledger support in axelard
    
    * update binary script
    
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -2,14 +2,15 @@
 
 platforms="$(go tool dist list | grep 'darwin\|linux' | grep 'amd64\|arm')"
 version="$1"
-ldflags="$2"
+build_tags="$2"
+ldflags="$3"
 echo "Building version $version"
 for platform in $platforms
 do
     arch="$(echo "$platform" | awk -F/ '{print $NF}')"
     os="$(echo "$platform" | awk -F/ '{print $(NF-1)}')"
     echo "Building binary for OS $os Architecture $arch"
-    GOOS=$os GOARCH=$arch go build -o ./bin/axelard-"$os"-"$arch"-"$version" -mod=readonly -ldflags "'$ldflags'" ./cmd/axelard
+    GOOS=$os GOARCH=$arch go build -o ./bin/axelard-"$os"-"$arch"-"$version" -mod=readonly -tags "$build_tags" -ldflags "'$ldflags'" ./cmd/axelard
 done
 
 cd bin || exit 1
