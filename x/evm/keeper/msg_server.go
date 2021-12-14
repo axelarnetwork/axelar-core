c79b3e60dce0de4955bcff3af7959b03218f717d
jcs47 <joao@axelar.network>
2021-12-14 01:48:52 +0000

    Update x/evm/keeper/msg_server.go
    Co-authored-by: Christian Gorenflo <christian@axelar.network>

--- x/evm/keeper/msg_server.go
+++ x/evm/keeper/msg_server.go
@@ -983,8 +983,8 @@ func (s msgServer) VoteConfirmTransferKey(c context.Context, req *types.VoteConf
 		return nil, err
 	}
 
-	s.Logger(ctx).Info(fmt.Sprintf("successfully confirmed %s key transfer for chain %s (tx ID '%s', rotation count '%d')",
-		keyRole.SimpleString(), chain.Name, pendingTransfer.TxID.Hex(), s.tss.GetRotationCount(ctx, chain, keyRole)))
+	s.Logger(ctx).Info(fmt.Sprintf("successfully confirmed %s key transfer for chain %s",
+		keyRole.SimpleString(), chain.Name), "txID", pendingTransfer.TxID.Hex(), "rotation count", s.tss.GetRotationCount(ctx, chain, keyRole)))
 	return &types.VoteConfirmTransferKeyResponse{}, nil
 }
 
