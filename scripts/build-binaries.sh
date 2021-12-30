77fd65de88c70bcec764b2194fb02d8546c03dff
Talal Ashraf <talal@axelar.network>
2021-12-30 23:45:53 +0500

    chore: backport commits from main with fixes for keyring dir, bitcoin node and version population (#1173)
    * fix: overwrite flag defaults correctly (#1171)
    
    * fix: overwrite flag defaults correctly
    
    * fix
    
    * chore: remove bitcoin from vald (#1170)
    
    * fix: binaries don't have the correct version and app name misconfigured (#1168)
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 platforms="$(go tool dist list | grep 'darwin\|linux' | grep 'amd64\|arm')"
-version="$1"
-build_tags="$2"
-ldflags="$3"
+version="${1}"
+build_tags="${2}"
+ldflags="${3}"
 echo "Building version $version"
 for platform in $platforms
 do
     arch="$(echo "$platform" | awk -F/ '{print $NF}')"
     os="$(echo "$platform" | awk -F/ '{print $(NF-1)}')"
     echo "Building binary for OS $os Architecture $arch"
-    GOOS=$os GOARCH=$arch go build -o ./bin/axelard-"$os"-"$arch"-"$version" -mod=readonly -tags "$build_tags" -ldflags "'$ldflags'" ./cmd/axelard
+    echo "ld_flags: ${ldflags}"
+    echo "build_tags: ${build_tags}"
+    echo "version: ${version}"
+    GOOS=$os GOARCH=$arch go build -o ./bin/axelard-"$os"-"$arch"-"$version" -mod=readonly -tags "$build_tags" -ldflags "$ldflags" ./cmd/axelard
 done
 
 cd bin || exit 1
