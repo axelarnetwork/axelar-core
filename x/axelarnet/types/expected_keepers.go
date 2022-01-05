96a151bcf31ab5cb8f35bab2bae270bc2a250a1d
João Sousa <joaosousa@MBP-de-Joao.home>
2022-01-05 22:33:06 +0000

    the confirm deposit command will now escrow all of the funds from the deposit account instead of only the specified amount

--- x/axelarnet/types/expected_keepers.go
+++ x/axelarnet/types/expected_keepers.go
@@ -60,6 +60,7 @@ type BankKeeper interface {
 	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
 }
 
 // IBCTransferKeeper provides functionality to manage IBC transfers
