83bc1b88eb33767ca2b31b9b59835c5846dd4b58
Sammy Liu <sammy.liu@axelar.network>
2022-01-20 00:36:07 +0800

    feat(vald): handle non-standard json-rpc (#1235) (#1237)

--- cmd/axelard/cmd/vald/evm/rpc/types.go
+++ cmd/axelard/cmd/vald/evm/rpc/types.go
@@ -1,6 +1,8 @@
 package rpc
 
 import (
+	"encoding/json"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
@@ -11,4 +13,21 @@ type MoonbeamHeader struct {
 	ExtrinsicsRoot common.Hash  `json:"extrinsicsRoot"   gencodec:"required"`
 	StateRoot      common.Hash  `json:"stateRoot"        gencodec:"required"`
 	Number         *hexutil.Big `json:"number"           gencodec:"required"`
+}
+
+// A value of this type can a JSON-RPC request, notification, successful response or
+// error response. Which one it is depends on the fields.
+type jsonrpcMessage struct {
+	Version string          `json:"jsonrpc,omitempty"`
+	ID      json.RawMessage `json:"id,omitempty"`
+	Method  string          `json:"method,omitempty"`
+	Params  json.RawMessage `json:"params,omitempty"`
+	Error   *jsonError      `json:"error,omitempty"`
+	Result  json.RawMessage `json:"result,omitempty"`
+}
+
+type jsonError struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
