43e3940a93739449975019dc0703161c85ea0346
Kiryl Yermakou <rma4ok@gmail.com>
2022-01-28 12:59:40 -0500

    feature(populate-bytecode): Adding support for Absorber

--- scripts/populate-bytecode.sh
+++ scripts/populate-bytecode.sh
@@ -2,27 +2,29 @@
 
 artifactsPath="contract-artifacts/gateway"
 
-singlesigGatewayFile="$artifactsPath/AxelarGatewayProxySinglesig.json"
-multisigGatewayFile="$artifactsPath/AxelarGatewayProxyMultisig.json"
-tokenFile="$artifactsPath/BurnableMintableCappedERC20.json"
-burnableFile="$artifactsPath/Burner.json"
-
-if [ ! -f $singlesigGatewayFile ] || [ ! -f $multisigGatewayFile ] || [ ! -f $tokenFile ] || [ ! -f $burnableFile ]; then
-    echo "Error: Contract files not found in $artifactsPath"
-    exit 1
-fi
-
-singlesigGateway="$(cat $singlesigGatewayFile | jq -r '.bytecode')"
-multisigGateway="$(cat $multisigGatewayFile | jq -r '.bytecode')"
-token="$(cat $tokenFile | jq -r '.bytecode')"
-burnable="$(cat $burnableFile | jq -r '.bytecode')"
+declare -a contracts=(
+    "AxelarGatewayProxySinglesig"
+    "AxelarGatewayProxyMultisig"
+    "BurnableMintableCappedERC20"
+    "Burner"
+    "Absorber"
+)
 
 cp x/evm/types/contracts.go.template x/evm/types/contracts.go
 
-sed -i.bak "s/%AxelarGatewayProxySinglesig_bytecode%/$singlesigGateway/g" x/evm/types/contracts.go
-sed -i.bak "s/%AxelarGatewayProxyMultisig_bytecode%/$multisigGateway/g" x/evm/types/contracts.go
-sed -i.bak "s/%BurnableMintableCappedERC20_bytecode%/$token/g" x/evm/types/contracts.go
-sed -i.bak "s/%Burner_bytecode%/$burnable/g" x/evm/types/contracts.go
+for contract in "${contracts[@]}"
+do
+  if [ ! -f "$artifactsPath/$contract.json" ]; then
+      echo "Error: Contract file $contract.json not found in $artifactsPath"
+      exit 1
+  fi
+
+  echo "Populating $contract"
+
+  bytecode="$(cat "$artifactsPath/$contract.json" | jq -r '.bytecode')"
+
+  sed -i.bak "s/%${contract}_bytecode%/$bytecode/g" x/evm/types/contracts.go
+done
 
 rm x/evm/types/contracts.go.bak
 
