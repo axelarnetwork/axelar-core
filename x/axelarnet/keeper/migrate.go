741ed276ee3a0f409993680e85b5bf305eef0da7
Haiyi Zhong <haiyi@axelar.network>
2022-02-04 01:30:14 -0500

    fix: cosmos chain struct
    cosmos chains were not set properly, need rebuild before we clean other states

--- x/axelarnet/keeper/migrate.go
+++ x/axelarnet/keeper/migrate.go
@@ -2,8 +2,11 @@ package keeper
 
 import (
 	"github.com/axelarnetwork/axelar-core/utils"
+	"github.com/axelarnetwork/axelar-core/x/axelarnet/exported"
 	"github.com/axelarnetwork/axelar-core/x/axelarnet/legacy"
+	"github.com/axelarnetwork/axelar-core/x/axelarnet/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strings"
 )
 
 // GetMigrationHandler returns the handler that performs in-place store migrations from v0.13 to v0.14. The
@@ -12,6 +15,8 @@ import (
 // - remove all chain <-> asset mapping
 func GetMigrationHandler(k Keeper) func(ctx sdk.Context) error {
 	return func(ctx sdk.Context) error {
+		// rebuild cosmos chain
+		rebuildCosmosChain(ctx, k)
 		// delete all path
 		deleteByPrefix(ctx, k, legacy.PathPrefix)
 		// delete chain by asset
@@ -24,10 +29,52 @@ func GetMigrationHandler(k Keeper) func(ctx sdk.Context) error {
 
 }
 
+func rebuildCosmosChain(ctx sdk.Context, k Keeper) {
+	for _, chainName := range k.GetCosmosChains(ctx) {
+		ibcPath := getIBCPath(ctx, k, chainName)
+		assets := getAssets(ctx, k, chainName)
+		chain, _ := k.GetCosmosChainByName(ctx, chainName)
+		k.SetCosmosChain(ctx, types.CosmosChain{
+			Name: chainName,
+			// used in migration
+			Assets:     append(chain.Assets, assets...),
+			IBCPath:    ibcPath,
+			AddrPrefix: chain.AddrPrefix,
+		})
+
+	}
+}
+
 func deleteByPrefix(ctx sdk.Context, k Keeper, keyPrefix utils.StringKey) {
 	iter := k.getStore(ctx).Iterator(keyPrefix)
 
 	for ; iter.Valid(); iter.Next() {
 		k.getStore(ctx).Delete(iter.GetKey())
 	}
+}
+
+func getIBCPath(ctx sdk.Context, k Keeper, chain string) string {
+	bz := k.getStore(ctx).GetRaw(legacy.PathPrefix.Append(utils.LowerCaseKey(chain)))
+	if bz == nil {
+		return ""
+	}
+
+	return string(bz)
+}
+
+func getAssets(ctx sdk.Context, k Keeper, chain string) []types.Asset {
+	iter := k.getStore(ctx).Iterator(legacy.AssetByChainPrefix.Append(utils.LowerCaseKey(chain)))
+
+	var assets []types.Asset
+	for ; iter.Valid(); iter.Next() {
+		var asset types.Asset
+		iter.UnmarshalValue(&asset)
+		// uaxl accidentally registered as native asset from terra
+		if asset.Denom == exported.NativeAsset && !strings.EqualFold(chain, exported.Axelarnet.Name) {
+			continue
+		}
+		assets = append(assets, asset)
+	}
+
+	return assets
 }
--- x/axelarnet/keeper/migrate_test.go
+++ x/axelarnet/keeper/migrate_test.go
@@ -14,9 +14,76 @@ import (
 	"github.com/axelarnetwork/axelar-core/testutils/rand"
 	"github.com/axelarnetwork/axelar-core/utils"
 	"github.com/axelarnetwork/axelar-core/x/axelarnet/legacy"
+	"github.com/axelarnetwork/axelar-core/x/axelarnet/types"
 )
 
-func TestStoreMigration(t *testing.T) {
+func TestGetMigrationHandler_rebuildCosmosChain(t *testing.T) {
+	encCfg := appParams.MakeEncodingConfig()
+	axelarnetSubspace := params.NewSubspace(encCfg.Codec, encCfg.Amino, sdk.NewKVStoreKey("axelarnetKey"), sdk.NewKVStoreKey("tAxelarnetKey"), "axelarnet")
+	ctx := sdk.NewContext(fake.NewMultiStore(), tmproto.Header{}, false, log.TestingLogger())
+	keeper := NewKeeper(encCfg.Codec, sdk.NewKVStoreKey("axelarnet"), axelarnetSubspace)
+
+	// create rand cosmos chains
+	randChains := make([]types.CosmosChain, rand.I64Between(10, 50))
+	for i := 0; i < len(randChains); i++ {
+		chain := randomChain()
+
+		// mock the state
+		// set chain
+		keeper.SetCosmosChain(ctx, types.CosmosChain{
+			Name:       chain.Name,
+			AddrPrefix: chain.AddrPrefix,
+		})
+		// set path
+		keeper.getStore(ctx).SetRaw(legacy.PathPrefix.Append(utils.LowerCaseKey(chain.Name)), []byte(chain.IBCPath))
+		for _, asset := range chain.Assets {
+			// set chain by asset
+			keeper.getStore(ctx).SetRaw(legacy.ChainByAssetPrefix.
+				Append(utils.LowerCaseKey(asset.Denom)), []byte(chain.Name))
+
+			// set asset by chain
+			keeper.getStore(ctx).Set(legacy.AssetByChainPrefix.
+				Append(utils.LowerCaseKey(chain.Name)).
+				Append(utils.LowerCaseKey(asset.Denom)), &asset)
+		}
+
+		randChains[i] = chain
+	}
+
+	for _, chain := range randChains {
+		// should get cosmos chain
+		c, ok := keeper.GetCosmosChainByName(ctx, chain.Name)
+		assert.True(t, ok)
+		assert.Equal(t, types.CosmosChain{Name: chain.Name, AddrPrefix: chain.AddrPrefix}, c)
+
+		// should get path
+		bz := keeper.getStore(ctx).GetRaw(legacy.PathPrefix.Append(utils.LowerCaseKey(chain.Name)))
+		assert.NotNil(t, bz)
+		assert.ElementsMatch(t, chain.Assets, getAssets(ctx, keeper, chain.Name))
+		for _, asset := range chain.Assets {
+			// should get chain by asset
+			bz := keeper.getStore(ctx).GetRaw(legacy.ChainByAssetPrefix.Append(utils.LowerCaseKey(asset.Denom)))
+			assert.NotNil(t, bz)
+		}
+	}
+
+	handler := GetMigrationHandler(keeper)
+	err := handler(ctx)
+	assert.NoError(t, err)
+
+	for _, chain := range randChains {
+		// should get cosmos chain
+		updated, ok := keeper.GetCosmosChainByName(ctx, chain.Name)
+		assert.True(t, ok)
+		assert.Equal(t, chain.Name, updated.Name)
+		assert.Equal(t, chain.IBCPath, updated.IBCPath)
+		assert.Equal(t, chain.AddrPrefix, updated.AddrPrefix)
+		assert.ElementsMatch(t, chain.Assets, updated.Assets)
+	}
+
+}
+
+func TestGetMigrationHandler_deletePrefix(t *testing.T) {
 
 	encCfg := appParams.MakeEncodingConfig()
 	axelarnetSubspace := params.NewSubspace(encCfg.Codec, encCfg.Amino, sdk.NewKVStoreKey("axelarnetKey"), sdk.NewKVStoreKey("tAxelarnetKey"), "axelarnet")
@@ -96,4 +163,25 @@ func TestStoreMigration(t *testing.T) {
 		bz := keeper.getStore(ctx).GetRaw(key)
 		assert.Nil(t, bz)
 	}
+}
+
+func randomChain() types.CosmosChain {
+	assets := make([]types.Asset, rand.I64Between(10, 50))
+	for i := 0; i < len(assets); i++ {
+		assets[i] = randAsset()
+	}
+	return types.CosmosChain{
+		Name:       rand.Denom(5, 10),
+		IBCPath:    rand.Str(5) + "/" + rand.Str(5),
+		AddrPrefix: rand.Denom(5, 10),
+		Assets:     assets,
+	}
+}
+
+func randAsset() types.Asset {
+	return types.Asset{
+		Denom:     rand.Denom(5, 10),
+		MinAmount: sdk.NewInt(rand.I64Between(100000, 1000000)),
+	}
+
 }
