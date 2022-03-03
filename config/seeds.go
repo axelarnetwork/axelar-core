c28ba3596a9c7fa7bd48dff51e9461543597736a
Christian Gorenflo <christian@axelar.network>
2022-03-03 14:34:56 -0500

    linting

--- config/seeds.go
+++ config/seeds.go
@@ -7,11 +7,13 @@ import (
 	"github.com/tendermint/tendermint/config"
 )
 
+// Seed is used to unmarshal the seeds.toml file
 type Seed struct {
 	Name    string `mapstructure:"name"`
 	Address string `mapstructure:"address"`
 }
 
+// ReadSeeds read seeds from a seeds.toml file in the config directory
 func ReadSeeds(v *viper.Viper) ([]string, error) {
 	v.SetConfigName("seeds")
 	v.SetConfigType("toml")
@@ -32,6 +34,7 @@ func ReadSeeds(v *viper.Viper) ([]string, error) {
 	return seeds, nil
 }
 
+// MergeSeeds adds the additional seeds to the existing config and ensures they are all distinct
 func MergeSeeds(cfg *config.Config, newSeeds []string) *config.Config {
 	seeds := append(strings.Split(cfg.P2P.Seeds, ","), newSeeds...)
 	cfg.P2P.Seeds = strings.Join(distinct(seeds), ",")
