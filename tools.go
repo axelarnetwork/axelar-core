34c89d9eb3216ce6242c9e6dde8504d0c06a1afe
Christian Gorenflo <christian@axelar.network>
2022-03-09 17:13:33 -0500

    fix

--- tools.go
+++ tools.go
@@ -1,4 +1,5 @@
 //go:build tools
+// +build tools
 
 // This is the canonical way to enforce dependency inclusion in go.mod for tools that are not directly involved in the build process
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
