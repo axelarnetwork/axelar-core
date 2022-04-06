05065046e3c405a4795615e374ad441ef42fe27f
Christian Gorenflo <christian@axelar.network>
2022-04-06 18:06:00 -0400

    lint

--- x/ante/types/mock/alias.go
+++ x/ante/types/mock/alias.go
@@ -7,7 +7,10 @@ import (
 
 //go:generate moq -pkg mock -out ./alias_mock.go . Tx Msg
 
+// Tx alias for mocking
 type Tx sdk.Tx
+
+// Msg alias for mocking
 type Msg interface {
 	sdk.Msg
 	descriptor.Message
