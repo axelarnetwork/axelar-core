ffec9a8defa61090b4c97431ba88b3c1624611e9
Christian Gorenflo <christian@axelar.network>
2022-04-05 22:44:33 -0400

    fix comment

--- x/permission/exported/proto.go
+++ x/permission/exported/proto.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 )
 
-// GetPermissionRole returns the role that is defined for the given message. Returns ROLE_CHAIN_MANAGEMENT if none is set.
+// GetPermissionRole returns the role that is defined for the given message. Returns ROLE_UNSPECIFIED if none is set.
 func GetPermissionRole(message descriptor.Message) Role {
 	_, d := descriptor.ForMessage(message)
 	v, err := proto.GetExtension(d.GetOptions(), E_PermissionRole)
