f0dbc450469911417bf088cf2ff2a252c6a22256
Milap Sheth <milap@axelar.network>
2022-04-21 20:11:20 -0400

    add log

--- cmd/axelard/cmd/vald/tss/tss.go
+++ cmd/axelard/cmd/vald/tss/tss.go
@@ -212,6 +212,7 @@ func (mgr *Mgr) RefreshKeys(ctx context.Context) error {
 
 	mgr.Keys = make(map[string][][]byte, len(valKeysResponse.Keys))
 	for keyID, keys := range valKeysResponse.Keys {
+		mgr.Logger.Info(fmt.Sprintf("retrieved key %s", keyID))
 		mgr.Keys[keyID] = keys.Keys
 	}
 
