9f659e0b04979253dbeec87ce4a09a0e770ea0c3
Sammy Liu <sammy.liu@axelar.network>
2022-04-25 22:49:27 +0800

    feat/delete uaxl token on all evm chains for re-deployment

--- x/evm/keeper/migrate.go
+++ x/evm/keeper/migrate.go
@@ -1,14 +1,20 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/axelarnetwork/axelar-core/utils"
 	"github.com/axelarnetwork/axelar-core/x/evm/types"
 )
 
+const uaxlAsset = "uaxl"
+
 // GetMigrationHandler returns the handler that performs in-place store migrations from v0.15 to v0.16. The
 // migration includes:
-// 	- delete pending gateways of all evm chains
+// - delete uaxl token for all evm chains
+// - delete uaxl token's burners for all evm chains
 func GetMigrationHandler(k types.BaseKeeper, n types.Nexus) func(ctx sdk.Context) error {
 	return func(ctx sdk.Context) error {
 		for _, chain := range n.GetChains(ctx) {
@@ -16,12 +22,71 @@ func GetMigrationHandler(k types.BaseKeeper, n types.Nexus) func(ctx sdk.Context
 				continue
 			}
 
-			keeper := k.ForChain(chain.Name).(chainKeeper)
-			if gateway := keeper.getGateway(ctx); gateway.Status == types.GatewayStatusPending {
-				keeper.getStore(ctx, keeper.chainLowerKey).Delete(gatewayKey)
+			ck := k.ForChain(chain.Name).(chainKeeper)
+			token, ok := ck.getTokenMetadataByAsset(ctx, uaxlAsset)
+			if !ok {
+				continue
+			}
+
+			deleteToken(ctx, ck, token)
+			deleteTokenBurners(ctx, ck)
+			migrateConfirmedDepositsToBurnt(ctx, ck)
+
+			if err := deleteTokenDeploymentCommand(ctx, ck, token); err != nil {
+				return err
 			}
 		}
 
 		return nil
+	}
+}
+
+func deleteToken(ctx sdk.Context, ck chainKeeper, token types.ERC20TokenMetadata) {
+	// delete lookup by asset
+	ck.getStore(ctx, ck.chainLowerKey).Delete(tokenMetadataByAssetPrefix.Append(utils.LowerCaseKey(token.Asset)))
+	// delete lookup by symbol
+	ck.getStore(ctx, ck.chainLowerKey).Delete(tokenMetadataBySymbolPrefix.Append(utils.LowerCaseKey(token.Details.Symbol)))
+}
+
+func deleteTokenDeploymentCommand(ctx sdk.Context, ck chainKeeper, token types.ERC20TokenMetadata) error {
+	chainID, ok := ck.GetChainID(ctx)
+	if !ok {
+		return fmt.Errorf("chain ID not found for chain %s", ck.GetName())
+	}
+
+	tokenDeploymentCommandID := types.NewCommandID([]byte(token.Details.Symbol), chainID)
+	if _, ok := ck.GetCommand(ctx, tokenDeploymentCommandID); !ok {
+		return fmt.Errorf("token deployment command not found for token %s and chain %s", token.Details.Symbol, ck.GetName())
+	}
+
+	ck.getStore(ctx, ck.chainLowerKey).Delete(commandPrefix.AppendStr(tokenDeploymentCommandID.Hex()))
+
+	return nil
+}
+
+func deleteTokenBurners(ctx sdk.Context, ck chainKeeper) {
+	iter := ck.getStore(ctx, ck.chainLowerKey).Iterator(burnerAddrPrefix)
+	defer utils.CloseLogError(iter, ck.Logger(ctx))
+
+	for ; iter.Valid(); iter.Next() {
+		var burner types.BurnerInfo
+		iter.UnmarshalValue(&burner)
+
+		if burner.Asset != uaxlAsset {
+			continue
+		}
+
+		ck.getStore(ctx, ck.chainLowerKey).Delete(iter.GetKey())
+	}
+}
+
+func migrateConfirmedDepositsToBurnt(ctx sdk.Context, ck chainKeeper) {
+	for _, deposit := range ck.GetConfirmedDeposits(ctx) {
+		if deposit.Asset != uaxlAsset {
+			continue
+		}
+
+		ck.DeleteDeposit(ctx, deposit)
+		ck.SetDeposit(ctx, deposit, types.DepositStatus_Burned)
 	}
 }
--- x/evm/keeper/migrate_test.go
+++ x/evm/keeper/migrate_test.go
@@ -5,7 +5,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramsKeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -13,11 +12,17 @@ import (
 	"github.com/axelarnetwork/axelar-core/app/params"
 	"github.com/axelarnetwork/axelar-core/testutils/fake"
 	"github.com/axelarnetwork/axelar-core/testutils/rand"
+	"github.com/axelarnetwork/axelar-core/x/evm/exported"
 	"github.com/axelarnetwork/axelar-core/x/evm/types"
 	"github.com/axelarnetwork/axelar-core/x/evm/types/mock"
-	"github.com/axelarnetwork/axelar-core/x/evm/types/testutils"
-	"github.com/axelarnetwork/axelar-core/x/nexus/exported"
+	testUtils "github.com/axelarnetwork/axelar-core/x/evm/types/testutils"
+	nexus "github.com/axelarnetwork/axelar-core/x/nexus/exported"
+	tssTestUtils "github.com/axelarnetwork/axelar-core/x/tss/exported/testutils"
+	"github.com/axelarnetwork/utils/slices"
+	. "github.com/axelarnetwork/utils/test"
 )
+
+const uaxl = "uaxl"
 
 func setup() (sdk.Context, types.BaseKeeper) {
 	encCfg := params.MakeEncodingConfig()
@@ -25,45 +30,212 @@ func setup() (sdk.Context, types.BaseKeeper) {
 	ctx := sdk.NewContext(fake.NewMultiStore(), tmproto.Header{}, false, log.TestingLogger())
 	keeper := NewKeeper(encCfg.Codec, sdk.NewKVStoreKey("evm"), paramsK)
 
+	for _, params := range types.DefaultParams() {
+		keeper.ForChain(params.Chain).SetParams(ctx, params)
+	}
+
 	return ctx, keeper
 }
 
-func TestGetMigrationHandler_deletePendingGateway(t *testing.T) {
-	ctx, keeper := setup()
-	evmChains := []exported.Chain{
+func TestGetMigrationHandler(t *testing.T) {
+	var (
+		ctx     sdk.Context
+		keeper  types.BaseKeeper
+		handler func(ctx sdk.Context) error
+	)
+
+	evmChains := []nexus.Chain{exported.Ethereum}
+	tokens := []types.ERC20TokenMetadata{
 		{
-			Name:   "evm-1",
-			Module: types.ModuleName,
+			Asset: "uaxl",
+			Details: types.TokenDetails{
+				TokenName: rand.NormalizedStr(5),
+				Symbol:    rand.NormalizedStr(5),
+				Decimals:  8,
+				Capacity:  sdk.ZeroInt(),
+			},
+			Status: types.Pending,
 		},
 		{
-			Name:   "evm-2",
-			Module: types.ModuleName,
-		},
-		{
-			Name:   "evm-3",
-			Module: types.ModuleName,
+			Asset: rand.NormalizedStr(5),
+			Details: types.TokenDetails{
+				TokenName: rand.NormalizedStr(5),
+				Symbol:    rand.NormalizedStr(5),
+				Decimals:  8,
+				Capacity:  sdk.ZeroInt(),
+			},
+			Status: types.Pending,
 		},
 	}
-	nexus := mock.NexusMock{
-		GetChainsFunc: func(_ sdk.Context) []exported.Chain {
-			return evmChains
-		},
-	}
-	keeper.ForChain(evmChains[0].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20)))})
-	keeper.ForChain(evmChains[1].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20))), Status: types.GatewayStatusConfirmed})
-	keeper.ForChain(evmChains[2].Name).(chainKeeper).setGateway(ctx, types.Gateway{Address: types.Address(common.BytesToAddress(rand.Bytes(20))), Status: types.GatewayStatusPending})
 
-	for _, chain := range evmChains {
-		keeper.ForChain(chain.Name).SetParams(ctx, testutils.RandomParams())
-	}
+	whenTokensAreSetup := Given("the migration handler", func(t *testing.T) {
+		ctx, keeper = setup()
+		nexus := mock.NexusMock{
+			GetChainsFunc: func(_ sdk.Context) []nexus.Chain {
+				return evmChains
+			},
+		}
+		handler = GetMigrationHandler(keeper, &nexus)
+	}).
+		When("tokens are setup for evm chains", func(t *testing.T) {
+			for _, chain := range evmChains {
+				for _, token := range tokens {
+					keeper.ForChain(chain.Name).(chainKeeper).setTokenMetadata(ctx, token)
 
-	handler := GetMigrationHandler(keeper, &nexus)
-	handler(ctx)
+					_, ok := keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataByAsset(ctx, token.Asset)
+					assert.True(t, ok)
+					_, ok = keeper.ForChain(chain.Name).(chainKeeper).getTokenMetadataBySymbol(ctx, token.Details.Symbol)
+					assert.True(t, ok)
+				}
+			}
+		})
 
-	_, actual := keeper.ForChain(evmChains[0].Name).GetGatewayAddress(ctx)
-	assert.True(t, actual)
-	_, actual = keeper.ForChain(evmChains[1].Name).GetGatewayAddress(ctx)
-	assert.True(t, actual)
-	_, actual = keeper.ForChain(evmChains[2].Name).GetGatewayAddress(ctx)
-	assert.False(t, actual)
+	whenTokensAreSetup.
+		Then("should delete uaxl token", func(t *testing.T) {
+			err := handler(ctx)
+			assert.Error(t, err)
+
+			for _, chain := range evmChains {
+				ck := keeper.ForChain(chain.Name).(chainKeeper)
+
+				for _, token := range tokens {
+					switch token.Asset {
+					case uaxl:
+						_, ok := ck.getTokenMetadataByAsset(ctx, token.Asset)
+						assert.False(t, ok)
+						_, ok = ck.getTokenMetadataBySymbol(ctx, token.Details.Symbol)
+						assert.False(t, ok)
+					default:
+						_, ok := ck.getTokenMetadataByAsset(ctx, token.Asset)
+						assert.True(t, ok)
+						_, ok = ck.getTokenMetadataBySymbol(ctx, token.Details.Symbol)
+						assert.True(t, ok)
+					}
+				}
+			}
+		}).
+		Run(t)
+
+	chainToCommandIDs := make(map[string][]types.CommandID)
+	whenTokensAreSetup.
+		And().
+		When("token deployment commands are set", func(t *testing.T) {
+			for _, chain := range evmChains {
+				chainToCommandIDs[chain.Name] = make([]types.CommandID, len(tokens))
+
+				for i, meta := range tokens {
+					ck := keeper.ForChain(chain.Name).(chainKeeper)
+
+					meta.ChainID, _ = ck.GetChainID(ctx)
+					token := types.CreateERC20Token(func(_ types.ERC20TokenMetadata) {}, meta)
+
+					command, err := token.CreateDeployCommand(tssTestUtils.RandKeyID())
+					if err != nil {
+						panic(err)
+					}
+
+					if err := ck.EnqueueCommand(ctx, command); err != nil {
+						panic(err)
+					}
+
+					chainToCommandIDs[chain.Name][i] = command.ID
+
+					_, ok := ck.GetCommand(ctx, command.ID)
+					assert.True(t, ok)
+				}
+			}
+		}).
+		Then("should delete uaxl token deployment command", func(t *testing.T) {
+			err := handler(ctx)
+			assert.NoError(t, err)
+
+			for chain, commandIDs := range chainToCommandIDs {
+				for i, commandID := range commandIDs {
+					_, ok := keeper.ForChain(chain).GetCommand(ctx, commandID)
+
+					switch tokens[i].Asset {
+					case uaxl:
+						assert.False(t, ok)
+					default:
+						assert.True(t, ok)
+					}
+				}
+			}
+		}).
+		Run(t)
+
+	chainToNonUaxlBurnerCount := make(map[string]uint)
+	whenTokensAreSetup.
+		And().
+		When("some token burners are set", func(t *testing.T) {
+			for _, chain := range evmChains {
+				burnerCount := int(rand.I64Between(5, 1000))
+				ck := keeper.ForChain(chain.Name).(chainKeeper)
+
+				for i := 0; i < burnerCount; i++ {
+					switch rand.Bools(0.5).Next() {
+					case true:
+						ck.SetBurnerInfo(ctx, types.BurnerInfo{Asset: uaxl, BurnerAddress: testUtils.RandomAddress()})
+					default:
+						chainToNonUaxlBurnerCount[chain.Name]++
+						ck.SetBurnerInfo(ctx, types.BurnerInfo{Asset: rand.NormalizedStr(5), BurnerAddress: testUtils.RandomAddress()})
+					}
+				}
+
+				assert.Len(t, ck.getBurnerInfos(ctx), burnerCount)
+			}
+		}).
+		Then("should delete uaxl burner infos", func(t *testing.T) {
+			err := handler(ctx)
+			assert.Error(t, err)
+
+			for chain, count := range chainToNonUaxlBurnerCount {
+				ck := keeper.ForChain(chain).(chainKeeper)
+				assert.Len(t, ck.getBurnerInfos(ctx), int(count))
+			}
+		}).
+		Run(t)
+
+	chainToUaxlConfirmedDepositCount := make(map[string]uint)
+	whenTokensAreSetup.
+		And().
+		When("some confirmed uaxl deposits exist", func(t *testing.T) {
+			for _, chain := range evmChains {
+				confirmedDepositCount := int(rand.I64Between(5, 1000))
+				ck := keeper.ForChain(chain.Name).(chainKeeper)
+
+				for i := 0; i < confirmedDepositCount; i++ {
+					deposit := types.ERC20Deposit{
+						TxID:          testUtils.RandomHash(),
+						BurnerAddress: testUtils.RandomAddress(),
+					}
+
+					switch rand.Bools(0.5).Next() {
+					case true:
+						deposit.Asset = uaxl
+						chainToUaxlConfirmedDepositCount[chain.Name]++
+					default:
+						deposit.Asset = rand.NormalizedStr(5)
+					}
+
+					ck.SetDeposit(ctx, deposit, types.DepositStatus_Confirmed)
+				}
+
+				assert.Len(t, ck.GetConfirmedDeposits(ctx), confirmedDepositCount)
+				assert.Len(t, slices.Filter(ck.GetConfirmedDeposits(ctx), func(d types.ERC20Deposit) bool { return d.Asset == uaxl }), int(chainToUaxlConfirmedDepositCount[chain.Name]))
+			}
+		}).
+		Then("should migrate confirmed uaxl deposits to burnt", func(t *testing.T) {
+			err := handler(ctx)
+			assert.Error(t, err)
+
+			for chain, count := range chainToUaxlConfirmedDepositCount {
+				ck := keeper.ForChain(chain).(chainKeeper)
+
+				assert.NotEmpty(t, ck.GetConfirmedDeposits(ctx))
+				assert.Len(t, slices.Filter(ck.GetConfirmedDeposits(ctx), func(d types.ERC20Deposit) bool { return d.Asset == uaxl }), 0)
+				assert.Len(t, ck.getBurnedDeposits(ctx), int(count))
+			}
+		}).
+		Run(t)
 }
