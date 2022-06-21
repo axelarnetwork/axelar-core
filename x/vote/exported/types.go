c1255269455d9ebbd14d001aebea35b64cca3b92
Milap Sheth <milap@axelar.network>
2022-06-21 16:26:39 +0000

    fix(vote): don't allow late votes from non participants (#1533)
    * fix(vote): don't allow late votes from non participants
    
    * updated sys
    
    * address comments

--- x/vote/exported/types.go
+++ x/vote/exported/types.go
@@ -71,7 +71,7 @@ func (m PollMetadata) Validate() error {
 	actualTotalVotingPower := sdk.ZeroInt()
 	for _, voter := range m.Voters {
 		if err := sdk.VerifyAddressFormat(voter.Validator); err != nil {
-			return nil
+			return err
 		}
 
 		if voter.VotingPower <= 0 {
