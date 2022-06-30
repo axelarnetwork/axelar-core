a7722548307bf2ccdf2b9f63a9992770a2188ae5
Sammy Liu <sammy.liu@axelar.network>
2022-07-01 04:17:48 +0800

    remove amino

--- x/multisig/types/codec.go
+++ x/multisig/types/codec.go
@@ -1,12 +1,8 @@
 package types
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 )
-
-// RegisterLegacyAminoCodec registers concrete types on codec
-func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
 
 // RegisterInterfaces registers types and interfaces with the given registry
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {}
