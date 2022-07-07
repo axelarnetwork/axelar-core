f71d4653c8a1519927a4faf995ae2da558738909
Christian Gorenflo <christian@axelar.network>
2022-07-07 11:32:36 -0400

    refactor: bring the strings length limit in line with random integers

--- testutils/rand/rand.go
+++ testutils/rand/rand.go
@@ -44,12 +44,12 @@ func NormalizeString(str string) string {
 
 // NormalizedStr creates a random normalized string of the provided length
 func NormalizedStr(len int) string {
-	return NormalizedStrBetween(len, len)
+	return NormalizedStrBetween(len, len+1)
 }
 
-// NormalizedStrBetween creates a random normalized string in the provided range
-func NormalizedStrBetween(min, max int) string {
-	return strings.ReplaceAll(NormalizeString(StrBetween(min, max)), defaultDelimiter, "-")
+// NormalizedStrBetween creates a random normalized string in the provided range (exclusive uper limit)
+func NormalizedStrBetween(min, shorterThan int) string {
+	return strings.ReplaceAll(NormalizeString(StrBetween(min, shorterThan)), defaultDelimiter, "-")
 }
 
 // I64Gen represents an random integer generator to generate a sequence of integers with the same properties.
@@ -204,10 +204,10 @@ type StringGen struct {
 }
 
 // Strings returns a random string generator that produces strings from the default alphabet of random length in the given limits (inclusive)
-func Strings(minLength int, maxLength int) StringGen {
+func Strings(minLength int, shorterThan int) StringGen {
 	alphabet := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.:")
 	return StringGen{
-		lengthGen:  I64GenBetween(int64(minLength), int64(maxLength+1)),
+		lengthGen:  I64GenBetween(int64(minLength), int64(shorterThan)),
 		alphabet:   alphabet,
 		charPicker: I64GenBetween(0, int64(len(alphabet))),
 	}
@@ -216,7 +216,7 @@ func Strings(minLength int, maxLength int) StringGen {
 // Denom returns a random denom string
 func Denom(min, max int) string {
 	// first letter must be an ascii alphabet
-	return Strings(1, 1).WithAlphabet([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")).Next() + Strings(min, max).WithAlphabet([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-/")).Next()
+	return Strings(1, 2).WithAlphabet([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")).Next() + Strings(min, max).WithAlphabet([]rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-/")).Next()
 }
 
 // HexStrings returns a random hex string generator that produces hex strings with given length
@@ -231,14 +231,14 @@ func HexStrings(length int) StringGen {
 }
 
 // StrBetween returns a random string of random length in the given limits (inclusive)
-func StrBetween(minLength int, maxLength int) string {
-	g := Strings(minLength, maxLength)
+func StrBetween(minLength int, shorterThan int) string {
+	g := Strings(minLength, shorterThan)
 	return g.Next()
 }
 
 // Str returns a random string of given length
 func Str(len int) string {
-	return StrBetween(len, len)
+	return StrBetween(len, len+1)
 }
 
 // HexStr returns a random hex string of given length
--- x/tss/exported/testutils/rand.go
+++ x/tss/exported/testutils/rand.go
@@ -7,7 +7,7 @@ import (
 
 // RandKeyID creates a random valid key ID
 func RandKeyID() tss.KeyID {
-	keyID := tss.KeyID(rand.NormalizedStrBetween(tss.KeyIDLengthMin, tss.KeyIDLengthMax))
+	keyID := tss.KeyID(rand.NormalizedStrBetween(tss.KeyIDLengthMin, tss.KeyIDLengthMax+1))
 	if err := keyID.Validate(); err != nil {
 		panic(err)
 	}
