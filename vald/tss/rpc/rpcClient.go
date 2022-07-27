a60427c4950fbe3abba5cec97f34c755c43063f9
Milap Sheth <milap@axelar.network>
2022-07-27 19:13:09 +0400

    update deps

--- vald/tss/rpc/rpcClient.go
+++ vald/tss/rpc/rpcClient.go
@@ -3,7 +3,6 @@ package rpc
 import "github.com/axelarnetwork/axelar-core/x/tss/tofnd"
 
 //go:generate moq -pkg mock -out ./mock/rpcClient.go . MultiSigClient
-
 // MultiSigClient defines the interface of a grpc client to communicate with tofnd Multisig service
 type MultiSigClient interface {
 	tofnd.MultisigClient
