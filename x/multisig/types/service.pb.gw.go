ee768ba23486d0d667fa18e5fee81f042e585533
Milap Sheth <milap@axelar.network>
2022-08-11 02:45:45 +0000

    feat(multisig): add keygen session query (#1664)
    * feat(multisig): add snapshot query
    
    * remove rest param
    
    * add doc
    
    * fix test
    
    * rename snapshot query
    
    * ci(check): update git diff syntax in `Ensure EVM module migration when bytecode changes` check (#1674)
    
    * refactor: remove amount from evm query DepositState since it is not used (#1673)
    
    * refactor/remove amount from evm query DepositState since it is not used
    
    * fix test
    
    * fix generate
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    
    * refactor to keygen session query
    
    * addressed comments
    
    * refactor getParticipants
    
    * check keygen session on key query
    
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    Co-authored-by: Sammy Liu <sammy.liu@axelar.network>

--- x/multisig/types/service.pb.gw.go
+++ x/multisig/types/service.pb.gw.go
@@ -311,6 +311,42 @@ func local_request_QueryService_Key_0(ctx context.Context, marshaler runtime.Mar
 
 }
 
+var (
+	filter_QueryService_KeygenSession_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_QueryService_KeygenSession_0(ctx context.Context, marshaler runtime.Marshaler, client QueryServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq KeygenSessionRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_QueryService_KeygenSession_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.KeygenSession(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_QueryService_KeygenSession_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq KeygenSessionRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_QueryService_KeygenSession_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.KeygenSession(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterMsgServiceHandlerServer registers the http handlers for service MsgService to "mux".
 // UnaryRPC     :call MsgServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -463,6 +499,26 @@ func RegisterQueryServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		}
 
 		forward_QueryService_Key_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_QueryService_KeygenSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_QueryService_KeygenSession_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_QueryService_KeygenSession_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -708,6 +764,26 @@ func RegisterQueryServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
+	mux.Handle("GET", pattern_QueryService_KeygenSession_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_QueryService_KeygenSession_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_QueryService_KeygenSession_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -717,6 +793,8 @@ var (
 	pattern_QueryService_NextKeyID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"axelar", "multisig", "v1beta1", "next_key_id", "chain"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_QueryService_Key_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"axelar", "multisig", "v1beta1", "key"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_QueryService_KeygenSession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"axelar", "multisig", "v1beta1", "keygen_session"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -725,4 +803,6 @@ var (
 	forward_QueryService_NextKeyID_0 = runtime.ForwardResponseMessage
 
 	forward_QueryService_Key_0 = runtime.ForwardResponseMessage
+
+	forward_QueryService_KeygenSession_0 = runtime.ForwardResponseMessage
 )
