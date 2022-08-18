c1038d06b08397ad6d0a815766660dc42c7e5c30
Haiyi Zhong <haiyi@axelar.network>
2022-08-18 05:38:30 -0400

    feat(axelarnet): migration to v0.23

--- x/axelarnet/keeper/migrate.go
+++ x/axelarnet/keeper/migrate.go
@@ -1,10 +1,43 @@
 package keeper
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
-// GetMigrationHandler returns the handler that performs in-place store migrations
-func GetMigrationHandler() func(_ sdk.Context) error {
+	"github.com/axelarnetwork/axelar-core/utils"
+	"github.com/axelarnetwork/axelar-core/x/axelarnet/types"
+)
+
+// GetMigrationHandler returns the handler that performs in-place store migrations from v0.22 to v0.23. The
+// migration includes
+// - set existing IBC transfers status to completed
+// - remove transfer under failedTransfer prefix
+// - remove nonce
+func GetMigrationHandler(k Keeper) func(_ sdk.Context) error {
 	return func(ctx sdk.Context) error {
+		setIBCTransfersCompleted(ctx, k)
+		removeFailedTransfers(ctx, k)
+		removeNonce(ctx, k)
+
 		return nil
 	}
+}
+
+func setIBCTransfersCompleted(ctx sdk.Context, k Keeper) {
+	for _, t := range k.getIBCTransfers(ctx) {
+		t.Status = types.TransferCompleted
+		k.setTransfer(ctx, t)
+	}
+}
+
+func removeFailedTransfers(ctx sdk.Context, k Keeper) {
+	iter := k.getStore(ctx).IteratorNew(failedTransferPrefix)
+	defer utils.CloseLogError(iter, k.Logger(ctx))
+
+	for ; iter.Valid(); iter.Next() {
+		k.getStore(ctx).DeleteRaw(iter.Key())
+	}
+}
+
+func removeNonce(ctx sdk.Context, k Keeper) {
+	k.getStore(ctx).DeleteRaw(nonceKey.Bytes())
 }
