b4c97175bf4e5d0f6dcd4d83798d86f5b6c4b730
Christian Gorenflo <christian@axelar.network>
2022-08-23 18:44:50 -0400

    refactor(nexus)!: optimize keeper access for chain state (#1716)
    * refactor!(nexus): optimize keeper access for chain state
    
    Chain state is a large object, so in the best case we only want to load and persist it once instead of every time a change is made.
    
    * alternative implementation
    
    * remove unnecessary refactor
    
    * fix
    
    * lint
    
    * lint

--- x/nexus/keeper/keeper_test.go
+++ x/nexus/keeper/keeper_test.go
@@ -68,10 +68,9 @@ func init() {
 	keeper.SetRouter(addressValidator())
 }
 
-func TestKeeper(t *testing.T) {
+func TestChainKeeper(t *testing.T) {
 	var (
-		ctx    sdk.Context
-		keeper nexusKeeper.Keeper
+		ctx sdk.Context
 	)
 
 	repeats := 20
@@ -80,11 +79,11 @@ func TestKeeper(t *testing.T) {
 		encCfg := app.MakeEncodingConfig()
 		nexusSubspace := params.NewSubspace(encCfg.Codec, encCfg.Amino, sdk.NewKVStoreKey("nexusKey"), sdk.NewKVStoreKey("tNexusKey"), "nexus")
 		ctx = sdk.NewContext(fake.NewMultiStore(), tmproto.Header{}, false, log.TestingLogger())
-		keeper = nexusKeeper.NewKeeper(encCfg.Codec, sdk.NewKVStoreKey("nexus"), nexusSubspace)
-		keeper.SetParams(ctx, types.DefaultParams())
+		k := nexusKeeper.NewKeeper(encCfg.Codec, sdk.NewKVStoreKey("nexus"), nexusSubspace)
+		k.SetParams(ctx, types.DefaultParams())
 	})
 
-	t.Run("MarkChainMaintainerMissingVote", testutils.Func(func(t *testing.T) {
+	t.Run("MarkMissingVote", testutils.Func(func(t *testing.T) {
 		var (
 			chain      exported.Chain
 			maintainer sdk.ValAddress
@@ -100,7 +99,9 @@ func TestKeeper(t *testing.T) {
 				}
 			}).
 			Then("should mark missing vote", func(t *testing.T) {
-				keeper.MarkChainMaintainerMissingVote(ctx, chain, maintainer, true)
+				state := keeper.GetChainState(ctx, chain)
+				state.MarkMissingVote(maintainer, true)
+				keeper.SetChainState(ctx, state)
 
 				maintainerStates := keeper.GetChainMaintainerStates(ctx, chain)
 				assert.Len(t, maintainerStates, 1)
@@ -110,7 +111,7 @@ func TestKeeper(t *testing.T) {
 			})
 	}).Repeat(repeats))
 
-	t.Run("MarkChainMaintainerIncorrectVote", testutils.Func(func(t *testing.T) {
+	t.Run("MarkIncorrectVote", testutils.Func(func(t *testing.T) {
 		var (
 			chain      exported.Chain
 			maintainer sdk.ValAddress
@@ -126,7 +127,9 @@ func TestKeeper(t *testing.T) {
 				}
 			}).
 			Then("should mark missing vote", func(t *testing.T) {
-				keeper.MarkChainMaintainerIncorrectVote(ctx, chain, maintainer, true)
+				state := keeper.GetChainState(ctx, chain)
+				state.MarkIncorrectVote(maintainer, true)
+				keeper.SetChainState(ctx, state)
 
 				maintainerStates := keeper.GetChainMaintainerStates(ctx, chain)
 				assert.Len(t, maintainerStates, 1)
