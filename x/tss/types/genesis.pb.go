287215bca9e4fe5b9797746201ed086bc8e083a2
Sammy Liu <sammy.liu@axelar.network>
2022-09-08 23:11:32 +0800

    refactor(tss): removal

--- x/tss/types/genesis.pb.go
+++ x/tss/types/genesis.pb.go
@@ -5,7 +5,7 @@ package types
 
 import (
 	fmt "fmt"
-	exported "github.com/axelarnetwork/axelar-core/x/tss/exported"
+	_ "github.com/axelarnetwork/axelar-core/x/tss/exported"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
@@ -25,13 +25,7 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type GenesisState struct {
-	Params            Params               `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
-	KeyRecoveryInfos  []KeyRecoveryInfo    `protobuf:"bytes,2,rep,name=key_recovery_infos,json=keyRecoveryInfos,proto3" json:"key_recovery_infos"`
-	Keys              []exported.Key       `protobuf:"bytes,3,rep,name=keys,proto3" json:"keys"`
-	MultisigInfos     []MultisigInfo       `protobuf:"bytes,4,rep,name=multisig_infos,json=multisigInfos,proto3" json:"multisig_infos"`
-	ExternalKeys      []ExternalKeys       `protobuf:"bytes,5,rep,name=external_keys,json=externalKeys,proto3" json:"external_keys"`
-	Signatures        []exported.Signature `protobuf:"bytes,6,rep,name=signatures,proto3" json:"signatures"`
-	ValidatorStatuses []ValidatorStatus    `protobuf:"bytes,7,rep,name=validator_statuses,json=validatorStatuses,proto3" json:"validator_statuses"`
+	Params Params `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
 }
 
 func (m *GenesisState) Reset()         { *m = GenesisState{} }
@@ -74,33 +68,22 @@ func init() {
 func init() { proto.RegisterFile("axelar/tss/v1beta1/genesis.proto", fileDescriptor_7cbe32320e614977) }
 
 var fileDescriptor_7cbe32320e614977 = []byte{
-	// 416 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0xcf, 0x6b, 0xd4, 0x40,
-	0x14, 0xc7, 0x13, 0xbb, 0xae, 0x30, 0x6d, 0x45, 0x07, 0x0f, 0x61, 0x0f, 0xd3, 0xa0, 0xa0, 0xbd,
-	0x98, 0xd8, 0x7a, 0x11, 0x8f, 0x05, 0x11, 0x59, 0x2b, 0xd2, 0x05, 0x15, 0x2f, 0x61, 0x76, 0xfb,
-	0x1a, 0x43, 0x7e, 0x4c, 0x98, 0xf7, 0x12, 0x93, 0xff, 0xc2, 0x7f, 0xc1, 0xff, 0x66, 0x8f, 0x3d,
-	0x7a, 0x12, 0xdd, 0xfd, 0x47, 0xc4, 0xc9, 0x04, 0x52, 0x9a, 0xf6, 0x96, 0xbc, 0xf7, 0x79, 0x9f,
-	0xf9, 0xce, 0xf0, 0x98, 0x2f, 0x1b, 0xc8, 0xa4, 0x0e, 0x09, 0x31, 0xac, 0x8f, 0x96, 0x40, 0xf2,
-	0x28, 0x8c, 0xa1, 0x00, 0x4c, 0x30, 0x28, 0xb5, 0x22, 0xc5, 0x79, 0x47, 0x04, 0x84, 0x18, 0x58,
-	0x62, 0xf6, 0x28, 0x56, 0xb1, 0x32, 0xed, 0xf0, 0xff, 0x57, 0x47, 0xce, 0x0e, 0x46, 0x5c, 0xa5,
-	0xd4, 0x32, 0xb7, 0xaa, 0x99, 0x18, 0x01, 0xa8, 0x2d, 0xa1, 0xef, 0x3f, 0x1b, 0xf4, 0xa1, 0x29,
-	0x95, 0x26, 0x38, 0x1f, 0x03, 0x1f, 0xff, 0x9c, 0xb0, 0xbd, 0xb7, 0x5d, 0xca, 0x05, 0x49, 0x02,
-	0xfe, 0x8a, 0x4d, 0xbb, 0x93, 0x3c, 0xd7, 0x77, 0x0f, 0x77, 0x8f, 0x67, 0xc1, 0xf5, 0xd4, 0xc1,
-	0x47, 0x43, 0x9c, 0x4c, 0xd6, 0xbf, 0x0f, 0x9c, 0x33, 0xcb, 0xf3, 0xcf, 0x8c, 0xa7, 0xd0, 0x46,
-	0x1a, 0x56, 0xaa, 0x06, 0xdd, 0x46, 0x49, 0x71, 0xa1, 0xd0, 0xbb, 0xe3, 0xef, 0x1c, 0xee, 0x1e,
-	0x3f, 0x19, 0xb3, 0xcc, 0xa1, 0x3d, 0xb3, 0xf0, 0xbb, 0xe2, 0x42, 0x59, 0xdd, 0x83, 0xf4, 0x6a,
-	0x19, 0xf9, 0x6b, 0x36, 0x49, 0xa1, 0x45, 0x6f, 0xc7, 0xa8, 0xfc, 0xa1, 0xaa, 0xbf, 0xdb, 0xd0,
-	0x69, 0x3d, 0x66, 0x86, 0x9f, 0xb2, 0xfb, 0x79, 0x95, 0x51, 0x82, 0x49, 0x6c, 0x03, 0x4d, 0xae,
-	0x5b, 0xfa, 0xe1, 0x53, 0x4b, 0x0e, 0xd2, 0xec, 0xe7, 0x83, 0x1a, 0xf2, 0x39, 0xdb, 0x87, 0x86,
-	0x40, 0x17, 0x32, 0x8b, 0x4c, 0xa6, 0xbb, 0x37, 0xdb, 0xde, 0x58, 0x70, 0x0e, 0x6d, 0xff, 0x54,
-	0x7b, 0x30, 0xa8, 0xf1, 0xf7, 0x8c, 0x61, 0x12, 0x17, 0x92, 0x2a, 0x0d, 0xe8, 0x4d, 0x8d, 0xe9,
-	0xe9, 0xad, 0xb7, 0x5b, 0xf4, 0xb8, 0xf5, 0x0d, 0xe6, 0xf9, 0x17, 0xc6, 0x6b, 0x99, 0x25, 0xe7,
-	0x92, 0x94, 0x8e, 0x90, 0x24, 0x55, 0x08, 0xe8, 0xdd, 0xbb, 0xf9, 0xf9, 0x3f, 0xf5, 0xf4, 0xc2,
-	0xc0, 0x56, 0xf9, 0xb0, 0xbe, 0x5a, 0x06, 0x3c, 0xf9, 0xb0, 0xfe, 0x2b, 0x9c, 0xf5, 0x46, 0xb8,
-	0x97, 0x1b, 0xe1, 0xfe, 0xd9, 0x08, 0xf7, 0xc7, 0x56, 0x38, 0x97, 0x5b, 0xe1, 0xfc, 0xda, 0x0a,
-	0xe7, 0xeb, 0x8b, 0x38, 0xa1, 0x6f, 0xd5, 0x32, 0x58, 0xa9, 0x3c, 0xec, 0x4e, 0x29, 0x80, 0xbe,
-	0x2b, 0x9d, 0xda, 0xbf, 0xe7, 0x2b, 0xa5, 0x21, 0x6c, 0xcc, 0x2a, 0x9a, 0xcd, 0x5b, 0x4e, 0xcd,
-	0xea, 0xbd, 0xfc, 0x17, 0x00, 0x00, 0xff, 0xff, 0xfa, 0x2d, 0x77, 0x9b, 0x32, 0x03, 0x00, 0x00,
+	// 232 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x48, 0xac, 0x48, 0xcd,
+	0x49, 0x2c, 0xd2, 0x2f, 0x29, 0x2e, 0xd6, 0x2f, 0x33, 0x4c, 0x4a, 0x2d, 0x49, 0x34, 0xd4, 0x4f,
+	0x4f, 0xcd, 0x4b, 0x2d, 0xce, 0x2c, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x82, 0xa8,
+	0xd0, 0x2b, 0x29, 0x2e, 0xd6, 0x83, 0xaa, 0x90, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0x4b, 0xeb,
+	0x83, 0x58, 0x10, 0x95, 0x52, 0xf2, 0x58, 0xcc, 0x2a, 0x48, 0x2c, 0x4a, 0xcc, 0x85, 0x1a, 0x25,
+	0x25, 0x87, 0x45, 0x41, 0x49, 0x65, 0x41, 0x2a, 0x4c, 0x5e, 0x1d, 0x49, 0x3e, 0xb5, 0xa2, 0x20,
+	0xbf, 0xa8, 0x24, 0x35, 0x05, 0x9b, 0x42, 0x25, 0x0f, 0x2e, 0x1e, 0x77, 0x88, 0x23, 0x83, 0x4b,
+	0x12, 0x4b, 0x52, 0x85, 0x2c, 0xb8, 0xd8, 0x20, 0x16, 0x49, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x1b,
+	0x49, 0xe9, 0x61, 0x3a, 0x5a, 0x2f, 0x00, 0xac, 0xc2, 0x89, 0xe5, 0xc4, 0x3d, 0x79, 0x86, 0x20,
+	0xa8, 0x7a, 0x27, 0xbf, 0x13, 0x0f, 0xe5, 0x18, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e,
+	0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58,
+	0x8e, 0x21, 0xca, 0x20, 0x3d, 0xb3, 0x24, 0xa3, 0x34, 0x49, 0x2f, 0x39, 0x3f, 0x57, 0x1f, 0x62,
+	0x62, 0x5e, 0x6a, 0x49, 0x79, 0x7e, 0x51, 0x36, 0x94, 0xa7, 0x9b, 0x9c, 0x5f, 0x94, 0xaa, 0x5f,
+	0x01, 0x76, 0x30, 0xd8, 0x7d, 0x49, 0x6c, 0x60, 0x07, 0x1a, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff,
+	0xbc, 0x78, 0xb4, 0x6a, 0x58, 0x01, 0x00, 0x00,
 }
 
 func (m *GenesisState) Marshal() (dAtA []byte, err error) {
@@ -123,90 +106,6 @@ func (m *GenesisState) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.ValidatorStatuses) > 0 {
-		for iNdEx := len(m.ValidatorStatuses) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.ValidatorStatuses[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x3a
-		}
-	}
-	if len(m.Signatures) > 0 {
-		for iNdEx := len(m.Signatures) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.Signatures[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x32
-		}
-	}
-	if len(m.ExternalKeys) > 0 {
-		for iNdEx := len(m.ExternalKeys) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.ExternalKeys[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x2a
-		}
-	}
-	if len(m.MultisigInfos) > 0 {
-		for iNdEx := len(m.MultisigInfos) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.MultisigInfos[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x22
-		}
-	}
-	if len(m.Keys) > 0 {
-		for iNdEx := len(m.Keys) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.Keys[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x1a
-		}
-	}
-	if len(m.KeyRecoveryInfos) > 0 {
-		for iNdEx := len(m.KeyRecoveryInfos) - 1; iNdEx >= 0; iNdEx-- {
-			{
-				size, err := m.KeyRecoveryInfos[iNdEx].MarshalToSizedBuffer(dAtA[:i])
-				if err != nil {
-					return 0, err
-				}
-				i -= size
-				i = encodeVarintGenesis(dAtA, i, uint64(size))
-			}
-			i--
-			dAtA[i] = 0x12
-		}
-	}
 	{
 		size, err := m.Params.MarshalToSizedBuffer(dAtA[:i])
 		if err != nil {
@@ -239,42 +138,6 @@ func (m *GenesisState) Size() (n int) {
 	_ = l
 	l = m.Params.Size()
 	n += 1 + l + sovGenesis(uint64(l))
-	if len(m.KeyRecoveryInfos) > 0 {
-		for _, e := range m.KeyRecoveryInfos {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
-	if len(m.Keys) > 0 {
-		for _, e := range m.Keys {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
-	if len(m.MultisigInfos) > 0 {
-		for _, e := range m.MultisigInfos {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
-	if len(m.ExternalKeys) > 0 {
-		for _, e := range m.ExternalKeys {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
-	if len(m.Signatures) > 0 {
-		for _, e := range m.Signatures {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
-	if len(m.ValidatorStatuses) > 0 {
-		for _, e := range m.ValidatorStatuses {
-			l = e.Size()
-			n += 1 + l + sovGenesis(uint64(l))
-		}
-	}
 	return n
 }
 
@@ -343,210 +206,6 @@ func (m *GenesisState) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field KeyRecoveryInfos", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.KeyRecoveryInfos = append(m.KeyRecoveryInfos, KeyRecoveryInfo{})
-			if err := m.KeyRecoveryInfos[len(m.KeyRecoveryInfos)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Keys", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Keys = append(m.Keys, exported.Key{})
-			if err := m.Keys[len(m.Keys)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 4:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field MultisigInfos", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.MultisigInfos = append(m.MultisigInfos, MultisigInfo{})
-			if err := m.MultisigInfos[len(m.MultisigInfos)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 5:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ExternalKeys", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.ExternalKeys = append(m.ExternalKeys, ExternalKeys{})
-			if err := m.ExternalKeys[len(m.ExternalKeys)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 6:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Signatures", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Signatures = append(m.Signatures, exported.Signature{})
-			if err := m.Signatures[len(m.Signatures)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 7:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ValidatorStatuses", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowGenesis
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthGenesis
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.ValidatorStatuses = append(m.ValidatorStatuses, ValidatorStatus{})
-			if err := m.ValidatorStatuses[len(m.ValidatorStatuses)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
