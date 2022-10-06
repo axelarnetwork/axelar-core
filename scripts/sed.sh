78b9cf643d892ec61e5ff4b5db49e7ea1cefa67d
Christian Gorenflo <christian@axelar.network>
2022-10-06 17:34:17 -0400

    fix: correct sed.sh for use on Macs

--- scripts/sed.sh
+++ scripts/sed.sh
@@ -2,7 +2,7 @@
 
 # Because Mac OS's version of sed has a few kinks and bugs, this command cannot be run directly from inside the Makefile
 # shellcheck disable=SC1003
-if [[ "$OSTYPE" =~ "^darwin" ]]; then
+if [[ "$OSTYPE" =~ ^darwin ]]; then
     sed -i '' '4i\'$'\noption go_package = "github.com/confio/ics23/go";' "$1"
 else
     sed -i '4i\'$'\noption go_package = "github.com/confio/ics23/go";' "$1"
