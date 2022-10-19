f439f24a97d358fc063d35f062886b87ca72187f
Sammy Liu <sammy.liu@axelar.network>
2022-10-19 23:40:53 +0800

    refactor(vald): use LatestFinalizedBlockNumber and HeaderByNumber for EVM JSON-RPC clients (#1791)
    * refactor(vald): LatestFinalizedBlockNumber and HeaderByNumber for EVM JSON-RPC clients
    
    * fix header
    
    * rpc clients pass by reference
    
    * trim header
    
    * remove pending from toBlockNumArg

---
+++ vald/evm/rpc/moonbeam.go
@@ -0,0 +1,35 @@
+package rpc
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type moonbeamClient struct {
+	*ethereumClient
+}
+
+func newMoonbeamClient(ethereumClient *ethereumClient) (*moonbeamClient, error) {
+	client := &moonbeamClient{ethereumClient: ethereumClient}
+	if _, err := client.LatestFinalizedBlockNumber(context.Background(), 0); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (c *moonbeamClient) LatestFinalizedBlockNumber(ctx context.Context, _ uint64) (*big.Int, error) {
+	var blockHash common.Hash
+	if err := c.rpc.CallContext(ctx, &blockHash, "chain_getFinalizedHead"); err != nil {
+		return nil, err
+	}
+
+	var moonbeamHeader MoonbeamHeader
+	if err := c.rpc.CallContext(ctx, &moonbeamHeader, "chain_getHeader", blockHash); err != nil {
+		return nil, err
+	}
+
+	return moonbeamHeader.Number.ToInt(), nil
+}
