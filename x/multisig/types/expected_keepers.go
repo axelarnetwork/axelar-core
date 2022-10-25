107bfc141bde31450707f88075acf0590c9c05d0
Christian Gorenflo <christian@axelar.network>
2022-10-25 15:11:50 -0400

    lint

--- x/multisig/types/expected_keepers.go
+++ x/multisig/types/expected_keepers.go
@@ -29,6 +29,7 @@ type Keeper interface {
 	GetSigRouter() SigRouter
 }
 
+// KeygenParticipator can check if a participant opted out of future keygens
 type KeygenParticipator interface {
 	IsOptOut(ctx sdk.Context, participant sdk.AccAddress) bool
 }
