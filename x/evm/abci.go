71c6da828f2f826e5356017885f1d547f31a2d04
Milap Sheth <milap@axelar.network>
2023-02-15 22:54:12 +0400

    restrict axelar from being a valid call contract recipient

--- x/evm/abci.go
+++ x/evm/abci.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/axelarnetwork/axelar-core/utils"
 	"github.com/axelarnetwork/axelar-core/utils/events"
+	axelarnet "github.com/axelarnetwork/axelar-core/x/axelarnet/exported"
 	"github.com/axelarnetwork/axelar-core/x/evm/types"
 	multisig "github.com/axelarnetwork/axelar-core/x/multisig/exported"
 	nexus "github.com/axelarnetwork/axelar-core/x/nexus/exported"
@@ -247,6 +248,10 @@ func setMessageToNexus(ctx sdk.Context, n types.Nexus, event types.Event, asset 
 		)
 	default:
 		return fmt.Errorf("unsupported event type %T", event)
+	}
+
+	if message.Recipient.Chain.Name.Equals(axelarnet.Axelarnet.Name) {
+		return fmt.Errorf("%s is not a supported recipient", axelarnet.Axelarnet.Name)
 	}
 
 	return n.SetNewMessage(ctx, message)
