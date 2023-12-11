ece3263ff99f9c3e414226eabd21665cda9e4d61
xyuan96 <xyuan96@sina.com>
2023-12-11 10:00:01 -0800

    undo accidental newline edit

--- app/app.go
+++ app/app.go
@@ -515,6 +515,7 @@ func initAppModules(keepers *keeperCache, bApp *bam.BaseApp, encodingConfig axel
 			*getKeeper[feegrantkeeper.Keeper](keepers),
 			encodingConfig.InterfaceRegistry,
 		),
+
 		snapshot.NewAppModule(*getKeeper[snapKeeper.Keeper](keepers)),
 		multisig.NewAppModule(
 			*getKeeper[multisigKeeper.Keeper](keepers),
