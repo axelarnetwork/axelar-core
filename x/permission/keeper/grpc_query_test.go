f87bb34f3f3729b95f18757002ced79a4ab92d08
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-12-21 12:39:45 -0500

    use given-when-then structure for testing

--- x/permission/keeper/grpc_query_test.go
+++ x/permission/keeper/grpc_query_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/stretchr/testify/assert"
@@ -22,6 +23,8 @@ func TestGrpcQuery(t *testing.T) {
 		k              keeper.Keeper
 		ctx            sdk.Context
 		initialGenesis *types.GenesisState
+		governanceKey  multisig.LegacyAminoPubKey
+		params         types.Params
 	)
 
 	Given("a keeper",
@@ -29,7 +32,7 @@ func TestGrpcQuery(t *testing.T) {
 			subspace := paramstypes.NewSubspace(cfg.Codec, cfg.Amino, sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "permission")
 			k = keeper.NewKeeper(cfg.Codec, sdk.NewKVStoreKey(types.StoreKey), subspace)
 		}).
-		When("the state is initialized from a genesis state",
+		Given("a state that is initialized",
 			func() {
 				initialGenesis = types.NewGenesisState(types.Params{}, randomMultisigGovernanceKey(), randomGovAccounts())
 				assert.NoError(t, initialGenesis.Validate())
@@ -37,20 +40,42 @@ func TestGrpcQuery(t *testing.T) {
 				ctx = sdk.NewContext(fake.NewMultiStore(), tmproto.Header{}, false, log.TestingLogger())
 				k.InitGenesis(ctx, initialGenesis)
 			}).
-		Then("query the governance key",
-			func(t *testing.T) {
+		When("querying the governance key",
+			func() {
 				req := &types.QueryGovernanceKeyRequest{}
 				resp, err := k.GovernanceKey(sdk.WrapSDKContext(ctx), req)
 				assert.NotNil(t, resp)
 				assert.Nil(t, err)
-				assert.Equal(t, *initialGenesis.GovernanceKey, resp.GovernanceKey)
+				governanceKey = resp.GovernanceKey
 			}).
-		Then("query the params",
+		Then("return the expected key",
 			func(t *testing.T) {
+				assert.Equal(t, *initialGenesis.GovernanceKey, governanceKey)
+			}).Run(t, 10)
+
+	Given("a keeper",
+		func() {
+			subspace := paramstypes.NewSubspace(cfg.Codec, cfg.Amino, sdk.NewKVStoreKey("paramsKey"), sdk.NewKVStoreKey("tparamsKey"), "permission")
+			k = keeper.NewKeeper(cfg.Codec, sdk.NewKVStoreKey(types.StoreKey), subspace)
+		}).
+		Given("a state that is initialized",
+			func() {
+				initialGenesis = types.NewGenesisState(types.Params{}, randomMultisigGovernanceKey(), randomGovAccounts())
+				assert.NoError(t, initialGenesis.Validate())
+
+				ctx = sdk.NewContext(fake.NewMultiStore(), tmproto.Header{}, false, log.TestingLogger())
+				k.InitGenesis(ctx, initialGenesis)
+			}).
+		When("querying params",
+			func() {
 				req := &types.ParamsRequest{}
 				resp, err := k.Params(sdk.WrapSDKContext(ctx), req)
 				assert.NotNil(t, resp)
 				assert.Nil(t, err)
-				assert.Equal(t, initialGenesis.Params, resp.Params)
+				params = resp.Params
+			}).
+		Then("return the expected params",
+			func(t *testing.T) {
+				assert.Equal(t, initialGenesis.Params, params)
 			}).Run(t, 10)
 }
