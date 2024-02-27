8f9177b63c6bf5d95abde2fe5b5472d552e7fa31
João Sousa <joaosousa@Joaos-MBP.home>
2024-02-27 02:32:40 +0000

    use /Users/joaosousa env var

--- cmd/axelard/cmd/root.go
+++ cmd/axelard/cmd/root.go
@@ -206,9 +206,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 
 	starterFlags := func(startCmd *cobra.Command) {
 		crisis.AddModuleInitFlags(startCmd)
-
-		homePath, _ := startCmd.Flags().GetString(flags.FlagHome)
-		startCmd.Flags().String(wasmdirFlag, filepath.Join(homePath, "wasm"), "path for the cosmwasm directory")
+		startCmd.Flags().String(wasmdirFlag, filepath.Join("$HOME", "."+app.Name, "wasm"), "path for the cosmwasm directory")
 	}
 
 	server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, export(encodingConfig), starterFlags)
