5a95e9af215b368f34863df0d793fe758cde2d08
Christian Gorenflo <christian@axelar.network>
2024-03-27 19:37:32 -0400

    fix(ante)!: ensure ante handlers can deal with no signers in msg

--- x/ante/restricted_tx.go
+++ x/ante/restricted_tx.go
@@ -24,17 +24,22 @@ func NewRestrictedTx(permission types.Permission) RestrictedTx {
 // AnteHandle fails if the signer is not authorized to send the transaction
 func (d RestrictedTx) AnteHandle(ctx sdk.Context, msgs []sdk.Msg, simulate bool, next MessageAnteHandler) (sdk.Context, error) {
 	for _, msg := range msgs {
-		signer := msg.GetSigners()[0]
-		signerRole := d.permission.GetRole(ctx, signer)
+		signers := msg.GetSigners()
+		var signer sdk.AccAddress
 
+		if len(signers) != 0 {
+			signer = signers[0]
+		}
+
+		signerRole := d.permission.GetRole(ctx, signer)
 		switch permission.GetPermissionRole((msg).(descriptor.Message)) {
 		case permission.ROLE_ACCESS_CONTROL:
 			if permission.ROLE_ACCESS_CONTROL != signerRole {
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "%s is not authorized to send transaction %T", signer, msg)
+				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "signer '%s' is not authorized to send transaction %T", signer, msg)
 			}
 		case permission.ROLE_CHAIN_MANAGEMENT:
 			if permission.ROLE_CHAIN_MANAGEMENT != signerRole {
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "%s is not authorized to send transaction %T", signer, msg)
+				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "signer '%s' is not authorized to send transaction %T", signer, msg)
 			}
 		default:
 			continue
