fa9d0331cdd43aea0cd71b185d00b9874bad9668
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2026-06-09 14:39:38 -0400

    fix(vald): migrate config decode hooks to go-viper/mapstructure/v2
    viper v1.21 (pulled in by cosmos-sdk v0.53) moved its DecoderConfigOption
    to the go-viper/mapstructure/v2 fork; the API is otherwise identical.

--- vald/config/decoding.go
+++ vald/config/decoding.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 
 	"github.com/axelarnetwork/axelar-core/vald/evm/rpc"
 )
